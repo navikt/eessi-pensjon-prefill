b86dc99d30375547461ba4a6d9317c7dd68706ec
Jens Madsen <jens.christian.madsen@nav.no>
2018-07-05 17:11:05 +0200

    EESSIPEN-11: Flyttet funskoner ut av fagmodul og inn i frontend.

--- src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/ApiController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/ApiController.kt
@@ -16,46 +16,46 @@ import org.springframework.web.bind.annotation.RequestMapping
 @RequestMapping("/api")
 class ApiController(private val euxService: EuxService) {
 
-    @GetMapping("/refreshAll")
-    fun refreshAll() {
-        euxService.refreshAll()
-    }
-
-    //saksid
-    @GetMapping("/case/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getCase(@PathVariable(value = "id", required = true) caseid: String): String {
-        return "{\"caseId\":\"$caseid\"}"
-    }
-
-    @ApiOperation("henter liste av alle tilgjengelige BuC fra EUX")
-    @GetMapping("/bucs", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun getBucs(): List<String> {
-        return euxService.getCachedBuCTypePerSekor()
-    }
-
-    @GetMapping("/seds", "/seds/{buc}")
-    fun getSeds(@PathVariable(value = "buc", required = false) buc: String?): List<String> {
-        return euxService.getAvailableSEDonBuc(buc)
-    }
-
-    @ApiOperation("henter liste av alle tilgjengelige instusjoner fra EUX")
-    @GetMapping("/institutions")
-    fun getInstitutions(@RequestParam(value = "buc", required = false, defaultValue = "") buc: String,
-                        @RequestParam(value = "landkode", required = false, defaultValue  = "") landkode: String): List<String> {
-        return euxService.getCachedInstitusjoner()
-        //return euxService.getInstitusjoner(buc, landkode)
-    }
-
-    @GetMapping("/institutions/{countrycode}")
-    fun getInstitutionsWithCountry(@PathVariable(value = "countrycode", required = false) landkode: String = ""): List<String> {
-        return euxService.getInstitusjoner("",landkode)
-    }
-
-    @AliasFor("/create")
-    @PostMapping("/casesubmit")
-    fun createDocumentSubmit(@RequestBody request: FrontendRequest): String {
-        return createDocument(request)
-    }
+//    @GetMapping("/refreshAll")
+//    fun refreshAll() {
+//        euxService.refreshAll()
+//    }
+//
+//    //saksid
+//    @GetMapping("/case/{id}", produces = [MediaType.APPLICATION_JSON_VALUE])
+//    fun getCase(@PathVariable(value = "id", required = true) caseid: String): String {
+//        return "{\"caseId\":\"$caseid\"}"
+//    }
+//
+//    @ApiOperation("henter liste av alle tilgjengelige BuC fra EUX")
+//    @GetMapping("/bucs", produces = [MediaType.APPLICATION_JSON_VALUE])
+//    fun getBucs(): List<String> {
+//        return euxService.getCachedBuCTypePerSekor()
+//    }
+//
+//    @GetMapping("/seds", "/seds/{buc}")
+//    fun getSeds(@PathVariable(value = "buc", required = false) buc: String?): List<String> {
+//        return euxService.getAvailableSEDonBuc(buc)
+//    }
+//
+//    @ApiOperation("henter liste av alle tilgjengelige instusjoner fra EUX")
+//    @GetMapping("/institutions")
+//    fun getInstitutions(@RequestParam(value = "buc", required = false, defaultValue = "") buc: String,
+//                        @RequestParam(value = "landkode", required = false, defaultValue  = "") landkode: String): List<String> {
+//        return euxService.getCachedInstitusjoner()
+//        //return euxService.getInstitusjoner(buc, landkode)
+//    }
+//
+//    @GetMapping("/institutions/{countrycode}")
+//    fun getInstitutionsWithCountry(@PathVariable(value = "countrycode", required = false) landkode: String = ""): List<String> {
+//        return euxService.getInstitusjoner("",landkode)
+//    }
+//
+//    @AliasFor("/create")
+//    @PostMapping("/casesubmit")
+//    fun createDocumentSubmit(@RequestBody request: FrontendRequest): String {
+//        return createDocument(request)
+//    }
 
     @ApiOperation("kjører prosess OpprettBuCogSED på EUX for å få dokuemt opprett i Rina")
     @PostMapping("/create")
--- src/main/kotlin/no/nav/eessi/eessifagmodul/services/EuxService.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/services/EuxService.kt
@@ -1,10 +1,6 @@
 package no.nav.eessi.eessifagmodul.services
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.google.common.cache.CacheBuilder
-import com.google.common.cache.CacheLoader
-import com.google.common.collect.Lists
-import com.google.common.collect.Sets
 import no.nav.eessi.eessifagmodul.models.RINASaker
 import no.nav.eessi.eessifagmodul.utils.createErrorMessage
 import no.nav.eessi.eessifagmodul.utils.typeRef
@@ -23,8 +19,6 @@ import org.springframework.util.MultiValueMap
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.io.IOException
-import java.util.concurrent.TimeUnit
-
 
 @Service
 @Description("Service class for EuxBasis - EuxCpiServiceController.java")
@@ -36,90 +30,8 @@ class EuxService(val oidcRestTemplate: RestTemplate) {
 
     private val objectMapper = jacksonObjectMapper()
 
-    private val buccache = CacheBuilder.newBuilder()
-            .expireAfterAccess(2, TimeUnit.HOURS)
-            .recordStats()
-            .build(object : CacheLoader<Any, List<String>>() {
-                @Throws(IOException::class)
-                override fun load(s: Any): List<String> {
-                    return getBuCtypePerSektor()
-                }
-            })
-
-    private val instituitioncache = CacheBuilder.newBuilder()
-            .expireAfterAccess(2, TimeUnit.HOURS)
-            .recordStats()
-            .build(object : CacheLoader<Any, List<String>>() {
-                @Throws(IOException::class)
-                override fun load(s: Any): List<String> {
-                    val result = getInstitusjoner()
-                    return result.sortedWith(compareBy({it},{it }))
-                }
-            })
-
-    //reload cache
-    fun refreshAll() {
-        try {
-            instituitioncache.refresh("")
-            logger.debug("instituitioncache refresh")
-            buccache.refresh("")
-            logger.debug("buccache refresh")
-        } catch (ex: Exception) {
-            logger.error("Something went wrong cache")
-        }
-    }
-
-    fun getBuC(euSaksnr: String): String? {
-//        val urlpath = "/BuC"
-//        val data = "?RINASaksnummer=$euSaksnr"
-//        return oidcRestTemplate.getForEntity<String>("$EUX_PATH$urlpath$data", typeRef<String>()).body
-        throw NotImplementedError("getBuC not yet implemented")
-    }
-
-    fun getCachedBuCTypePerSekor(): List<String> {
-        logger.debug("hotcount: ${buccache.stats().hitCount()} totalLoadtime: ${buccache.stats().totalLoadTime()}")
-        return buccache.get("")
-
-    }
-
-    fun getBuCtypePerSektor(): List<String> {
-        val urlPath = "/BuCTypePerSektor"
-
-        val headers = logonBasis()
-        val httpEntity = HttpEntity("", headers)
-        val response = oidcRestTemplate.exchange("$EUX_PATH$urlPath", HttpMethod.GET, httpEntity, typeRef<List<String>>())
-
-        return response.body!!
-    }
-
-    fun sendSED(euSaksnr: String, korrelasjonID: String, dokumentID: String): Boolean {
-//        val urlPath = "/SendSED"
-//        val data = "?RINASaksnummer=$euSaksnr&KorrelasjonsID=$korrelasjonID"
-//        return oidcRestTemplate.postForEntity<String>("$EUX_PATH$urlPath$data", typeRef<String>())
-        throw NotImplementedError("sendSED not yet implemented")
-    }
-
-    fun saveDocument(euSaksnr: String, korrelasjonID: String) {
-//        val data = "?RINASaksnummer=$euSaksnr&KorrelasjonsID=$korrelasjonID"
-//        val pathurl = "/SED"
-//        val response = oidcRestTemplate.postForEntity<String>("$EUX_PATH$pathurl$data", typeRef<Unit>())
-        throw NotImplementedError("saveDocument not yet implemented")
-    }
-
-    fun getDocument(euSaksnr: String, dokumentID: String): String {
-//        val data = "?RINASaksnummer=$euSaksnr&DokumentID=$dokumentID"
-//        val pathurl = "/SED"
-//        val response = oidcRestTemplate.getForEntity<String>("$EUX_PATH$pathurl$data", typeRef<String>())
-        throw NotImplementedError("getDocument not yet implemented")
-    }
-
-    fun updateDocument(euSaksnr: String, korrelasjonID: String, SEDType: String, dokumentID: String): String? {
-//        val data = "?RINASaksnummer=$euSaksnr&KorrelasjonsID=$korrelasjonID"
-//        val pathurl = "/SED"
-//        val response = oidcRestTemplate.postForEntity<String>("$EUX_PATH$pathurl$data", typeRef<Unit>())
-//        return response.body
-        throw NotImplementedError("updateDocument not yet implemented")
-    }
+    //test mock only
+    var overrideheaders: Boolean? = null
 
     //henter ut status på rina.
     fun getRinaSaker(bucType: String = "",rinaNummer: String? = ""): List<RINASaker> {
@@ -168,67 +80,6 @@ class EuxService(val oidcRestTemplate: RestTemplate) {
         }
     }
 
-    //Cached list of Institusjoner..
-    fun getCachedInstitusjoner(): List<String> {
-        val result = instituitioncache.get("")
-        return result
-    }
-
-    //PK-51002 --
-    //Henter ut en liste over registrerte institusjoner innenfor spesifiserte EU-land. PK-51002"
-    fun getInstitusjoner(bucType: String = "", landKode: String = ""): List<String> {
-        val urlPath = "/Institusjoner"
-
-        val builder = UriComponentsBuilder.fromPath("$EUX_PATH$urlPath")
-                .queryParam("BuCType", bucType)
-                .queryParam("LandKode", landKode)
-
-        val headers = logonBasis()
-        val httpEntity = HttpEntity("", headers)
-
-        val response = oidcRestTemplate.exchange(builder.toUriString(), HttpMethod.GET, httpEntity, typeRef<String>())
-        val responseBody = response.body!!
-        try {
-            if (response.statusCode.isError) {
-                throw createErrorMessage(responseBody)
-            } else {
-                return objectMapper.readValue(responseBody, typeRefs<List<String>>())
-            }
-        } catch (ex: IOException) {
-            throw RuntimeException()
-        }
-    }
-
-    //Henter ut en liste over alle SED typer som kan opprettes i sakens nåværende tilstand.
-    fun getAvailableSEDTypes(euSaksnr: String): List<String> {
-//        val urlPath = "/TilgjengeligeSEDTyper"
-//        val data = "?RINASaksnummer=$euSaksnr"
-//        val response = oidcRestTemplate.getForEntity<String>("$EUX_PATH$urlPath$data", typeRef<String>())
-        throw NotImplementedError()
-    }
-
-    fun getAvailableSEDonBuc(buc: String?): List<String> {
-        val list1 = listOf("P2000","P2200")
-        val list2 = listOf("P6000","P10000")
-        val list3 = listOf("P5000")
-
-        val map : Map<String, List<String>> =
-                mapOf(
-                        "P_BUC_01" to list1,
-                        "P_BUC_06" to list2,
-                        "P_BUC_07" to list3
-                )
-        if (buc.isNullOrEmpty()) {
-            val set: MutableSet<String> = Sets.newHashSet()
-            set.addAll(list1)
-            set.addAll(list2)
-            set.addAll(list3)
-            return Lists.newArrayList(set)
-        }
-
-        val result = map.get(buc).orEmpty()
-        return result
-    }
     /**
      * Call the orchestrator endpoint with necessary information to create a case in RINA, set
      * its receiver, create a document and add attachments to it.
@@ -272,8 +123,6 @@ class EuxService(val oidcRestTemplate: RestTemplate) {
         val response = oidcRestTemplate.exchange(builder.toUriString(), HttpMethod.POST, httpEntity, String::class.java)
         return response.body
     }
-
-    var overrideheaders: Boolean? = null
 
     //temp fuction to log system onto eux basis
     fun logonBasis(): HttpHeaders {
