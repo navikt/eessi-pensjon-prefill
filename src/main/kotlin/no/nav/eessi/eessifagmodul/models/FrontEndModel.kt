c47e4243eb035e54938df847cee845bba9d8a3dc
Jens Madsen <jens.christian.madsen@nav.no>
2018-07-06 10:11:02 +0200

    EESSIPEN-11: Flyttet funskoner ut av fagmodul og inn i frontend.

--- src/main/kotlin/no/nav/eessi/eessifagmodul/models/FrontEndModel.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/models/FrontEndModel.kt
@@ -1,9 +1,15 @@
 package no.nav.eessi.eessifagmodul.models
 
-//{"institution":"DUMMY","buc":"P_BUC_06","sed":"P6000","caseId":"caseId"}
+//{"institution":[{"NO:"DUMMY"}],"buc":"P_BUC_06","sed":"P6000","caseId":"caseId","subjectArea":"pensjon"}
 data class FrontendRequest(
-        val sed : String? = null,
-        val institution: String? = null,
+        val subjectAera: String? = null,
+        val caseId: String? = null,
         val buc: String? = null,
-        val caseId: String? = null
+        val sed : String? = null,
+        val institution: List<Institusjon>? = null
+)
+
+data class Institusjon(
+        val country: String? = null,
+        val institution: String? =null
 )
