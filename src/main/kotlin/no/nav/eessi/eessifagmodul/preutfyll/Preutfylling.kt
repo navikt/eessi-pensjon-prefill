4d0ef1777bff1f91474d6ed1506869d4301b6e28
Jens Madsen <jens.christian.madsen@nav.no>
2018-07-10 10:52:36 +0200

    EESSIPEN-11: Ferdigstilling av flytte logikk til frontend.

---
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/preutfyll/Preutfylling.kt
@@ -0,0 +1,16 @@
+package no.nav.eessi.eessifagmodul.preutfyll
+
+import no.nav.eessi.eessifagmodul.models.SED
+
+class Preutfylling{
+
+
+    fun preutfyllingAvSED(sed: SED, saknr: String, vedtak: String) : SED    {
+
+        //hente data fra PEN
+        //mappe om XML/data til onbje
+        return sed
+    }
+
+
+}
