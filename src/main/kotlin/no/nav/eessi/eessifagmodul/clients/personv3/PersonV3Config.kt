998f91a907d814e7c88d69cf8efe8de08ddea6c6
Tryti, Christian <christian.tryti@nav.no>
2018-07-11 11:32:38 +0200

    Lagt til personV3 klient. Requester konverterer oidc-tokens til SAML før kallet gjøres mot TPSWS. Refaktorert litt på mappe-strukturen

--- src/main/kotlin/no/nav/eessi/eessifagmodul/clients/personv3/PersonV3Config.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/clients/personv3/PersonV3Config.kt
@@ -1,24 +1,25 @@
-package no.nav.eessi.eessifagmodul.config.jaxws.client
+package no.nav.eessi.eessifagmodul.clients.personv3
 
-import no.nav.tjeneste.virksomhet.aktoer.v2.binding.AktoerV2
+import no.nav.tjeneste.virksomhet.person.v3.binding.PersonV3
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-class AktoerIdClientConfig {
+class PersonV3Config {
 
-    @Value("\${aktoer.v2.endpointurl}")
+    @Value("\${person.v3.endpointurl}")
     lateinit var endpointUrl: String
 
     @Bean
-    fun setupAktoerV2(): AktoerV2 {
+    fun personV3(): PersonV3 {
         val factory = JaxWsProxyFactoryBean()
-        factory.serviceClass = AktoerV2::class.java
+        factory.serviceClass = PersonV3::class.java
         factory.address = endpointUrl
         // Debug/logging av meldinger som sendes mellom app og tilbyder
         //factory.features.add(LoggingFeature()) // TODO: Add denne featureren bare dersom DEBUG er enabled
-        return factory.create() as AktoerV2
+        return factory.create() as PersonV3
     }
+
 }
