8b9c4d78dcf51c511900652aaa028ee512ea3c10
Tryti, Christian <christian.tryti@nav.no>
2018-08-28 11:30:53 +0200

    Legger til springfox i ignore-listen til token-support

--- src/main/kotlin/no/nav/eessi/eessifagmodul/EessiFagmodulApplication.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/EessiFagmodulApplication.kt
@@ -4,7 +4,7 @@ import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@EnableOIDCTokenValidation(ignore = ["org.springframework"])
+@EnableOIDCTokenValidation(ignore = ["org.springframework", "springfox.documentation"])
 @SpringBootApplication
 class EessiFagmodulApplication
 
