677dbdfa1d6e1587495fc7086a38f05e3690e666
Tryti, Christian <christian.tryti@nav.no>
2018-09-26 18:12:41 +0200

    Bumper antall retries til 30. Tar rundt 80 sec å opprette SED i RINA

--- src/main/kotlin/no/nav/eessi/eessifagmodul/services/eux/RinaActions.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/services/eux/RinaActions.kt
@@ -3,7 +3,6 @@ package no.nav.eessi.eessifagmodul.services.eux
 import no.nav.eessi.eessifagmodul.models.RINAaksjoner
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 
@@ -13,41 +12,36 @@ class RinaActions(private val euxService: EuxService) {
     private val logger: Logger by lazy { LoggerFactory.getLogger(RinaActions::class.java) }
 
     @Value("\${rinaaction.waittime:4000}")
-    lateinit var waittime : String  // waittime (basis venter 6000 på flere tjenester?)
+    lateinit var waittime: String  // waittime (basis venter 6000 på flere tjenester?)
 
-    var timeTries = 5               // times to try
+    var timeTries = 30               // times to try
 
     val create = "Create"
     val update = "Update"
 
-    fun canUpdate(sed: String, rinanr: String) : Boolean {
-        return isActionPossible(sed, rinanr, update, 5)
-    }
-    fun canCreate(sed: String, rinanr: String) : Boolean {
-        return isActionPossible(sed, rinanr, create,  5)
+    fun canUpdate(sed: String, rinanr: String): Boolean {
+        return isActionPossible(sed, rinanr, update, 1)
     }
 
-    fun isActionPossible(sed: String, rinanr: String, navn: String, deep: Int = 1) : Boolean {
-        logger.debug("Henter RINAaksjoner på sed: $sed, mot rinanr: $rinanr, letter etter: $navn og deep er: $deep")
-        var validCheck = false
+    fun canCreate(sed: String, rinanr: String): Boolean {
+        return isActionPossible(sed, rinanr, create, 5)
+    }
+
+    fun isActionPossible(sed: String, rinanr: String, navn: String, deep: Int = 1): Boolean {
+        logger.debug("Henter RINAaksjoner på sed: $sed, mot rinanr: $rinanr, leter etter: $navn og deep er: $deep")
         val result = getMuligeAksjoner(rinanr)
 
-        run breaker@ {
-            result.forEach {
-                if (sed == it.dokumentType && navn == it.navn) {
-                    validCheck = true
-                    return@breaker //exit foreatch
-                }
+        result.forEach {
+            if (sed == it.dokumentType && navn == it.navn) {
+                return true
             }
         }
-        if (validCheck) {
-            return validCheck
-        } else if (deep >= timeTries) {
-            return validCheck
+        if (deep >= timeTries) {
+            return false
         }
         logger.debug("Prøver igjen etter $waittime ms på å hente opp aksjoner.")
         Thread.sleep(waittime.toLong())
-        return isActionPossible(sed, rinanr, navn, deep+1)
+        return isActionPossible(sed, rinanr, navn, deep + 1)
     }
 
     private fun getMuligeAksjoner(rinanr: String): List<RINAaksjoner> {
