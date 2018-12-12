4f43d84b1e37fb630c36c1d5baf1421074821b1d
Mo Amini <mo.amini@nav.no>
2018-12-12 10:26:34 +0100

    fjernet hentperson counter midlertidig

--- src/main/kotlin/no/nav/eessi/eessifagmodul/services/personv3/PersonV3Service.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/services/personv3/PersonV3Service.kt
@@ -19,9 +19,9 @@ import org.springframework.stereotype.Component
 @Component
 class PersonV3Service(val service: PersonV3, val oidcRequestContextHolder: OIDCRequestContextHolder) {
 
-    val HENTPERSON_TELLER_NAVN = "eessipensjon_fagmodul.hentperson"
-    val HENTPERSON_TELLER_TYPE_VELLYKKEDE = counter(HENTPERSON_TELLER_NAVN, "vellykkede")
-    val HENTPERSON_TELLER_TYPE_FEILEDE = counter(HENTPERSON_TELLER_NAVN, "feilede")
+//    val HENTPERSON_TELLER_NAVN = "eessipensjon_fagmodul.hentperson"
+//    val HENTPERSON_TELLER_TYPE_VELLYKKEDE = counter(HENTPERSON_TELLER_NAVN, "vellykkede")
+//    val HENTPERSON_TELLER_TYPE_FEILEDE = counter(HENTPERSON_TELLER_NAVN, "feilede")
 
     fun counter(name: String, type: String): Counter {
         return Metrics.counter(name, "type", type)
@@ -43,13 +43,13 @@ class PersonV3Service(val service: PersonV3, val oidcRequestContextHolder: OIDCR
         }
         try {
             val resp = service.hentPerson(request)
-            HENTPERSON_TELLER_TYPE_VELLYKKEDE.increment()
+//            HENTPERSON_TELLER_TYPE_VELLYKKEDE.increment()
             return resp
         } catch (personIkkefunnet : HentPersonPersonIkkeFunnet) {
-            HENTPERSON_TELLER_TYPE_FEILEDE.increment()
+//            HENTPERSON_TELLER_TYPE_FEILEDE.increment()
             throw personIkkefunnet
         } catch (personSikkerhetsbegrensning: HentPersonSikkerhetsbegrensning) {
-            HENTPERSON_TELLER_TYPE_FEILEDE.increment()
+//            HENTPERSON_TELLER_TYPE_FEILEDE.increment()
             throw personSikkerhetsbegrensning
         }    }
 
