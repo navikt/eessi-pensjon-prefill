e40b1879d7134b02a8f956576266f21c47cbf8e3
Mo Amini <mo.amini@nav.no>
2019-06-13 18:40:56 +0200

    Lagt til safService

--- src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/SafController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/SafController.kt
@@ -1,11 +1,9 @@
 package no.nav.eessi.eessifagmodul.controllers
 
-import com.fasterxml.jackson.annotation.JsonInclude
 import io.swagger.annotations.ApiOperation
 import no.nav.eessi.eessifagmodul.services.saf.SafService
 import no.nav.security.oidc.api.Protected
 import org.slf4j.LoggerFactory
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 
 @Protected
@@ -16,8 +14,7 @@ class SafController(val safService: SafService) {
     private val logger = LoggerFactory.getLogger(SafController::class.java)
 
     @ApiOperation("Henter metadata for alle dokumenter i alle journalposter for en gitt aktørid")
-    @GetMapping("/metadata/{aktoerId}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @GetMapping("/metadata/{aktoerId}")
     fun getMetadata(@PathVariable("aktoerId", required = true) aktoerId: String): String {
         logger.info("Henter metadata for dokumenter i SAF for aktørid: $aktoerId")
         return safService.hentDokumentMetadata(aktoerId)
