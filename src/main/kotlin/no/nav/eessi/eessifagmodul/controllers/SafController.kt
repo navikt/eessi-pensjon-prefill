e92c733b092220ddd07bc6ab7b408f0d5f20097f
Mo Amini <mo.amini@nav.no>
2019-06-18 12:31:16 +0200

    fikset URI problem og returnerer korrekt statuscode

--- src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/SafController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/controllers/SafController.kt
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiOperation
 import no.nav.eessi.eessifagmodul.services.saf.SafService
 import no.nav.security.oidc.api.Protected
 import org.slf4j.LoggerFactory
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @Protected
@@ -15,7 +16,7 @@ class SafController(val safService: SafService) {
 
     @ApiOperation("Henter metadata for alle dokumenter i alle journalposter for en gitt aktørid")
     @GetMapping("/metadata/{aktoerId}")
-    fun getMetadata(@PathVariable("aktoerId", required = true) aktoerId: String): String {
+    fun getMetadata(@PathVariable("aktoerId", required = true) aktoerId: String): ResponseEntity<String> {
         logger.info("Henter metadata for dokumenter i SAF for aktørid: $aktoerId")
         return safService.hentDokumentMetadata(aktoerId)
     }
@@ -24,9 +25,8 @@ class SafController(val safService: SafService) {
     @GetMapping("/hentdokument/{journalpostId}/{dokumentInfoId}/{variantFormat}")
     fun getMetadata(@PathVariable("journalpostId", required = true) journalpostId: String,
                     @PathVariable("dokumentInfoId", required = true) dokumentInfoId: String,
-                    @PathVariable("variantFormat", required = true) variantFormat: String): String {
+                    @PathVariable("variantFormat", required = true) variantFormat: String): ResponseEntity<String> {
         logger.info("Henter dokumentinnhold fra SAF for journalpostId: $journalpostId, dokumentInfoId: $dokumentInfoId, variantFormat: $variantFormat")
         return safService.hentDokumentInnhold(journalpostId, dokumentInfoId, variantFormat)
     }
-
 }
--- src/main/kotlin/no/nav/eessi/eessifagmodul/services/saf/SafService.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/services/saf/SafService.kt
@@ -4,10 +4,7 @@ import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.Metrics
 import no.nav.eessi.eessifagmodul.utils.errorBody
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
-import org.springframework.http.MediaType
+import org.springframework.http.*
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import java.util.*
@@ -25,47 +22,50 @@ class SafService(val safGraphQlOidcRestTemplate: RestTemplate, val safRestOidcRe
         return Metrics.counter(name, "type", type)
     }
 
-    fun hentDokumentMetadata(aktoerId: String) : String {
+    fun hentDokumentMetadata(aktoerId: String) : ResponseEntity<String> {
          try {
              val headers = HttpHeaders()
              headers.contentType = MediaType.APPLICATION_JSON
              val httpEntity = HttpEntity(genererQuery(aktoerId), headers)
 
-             val response = safGraphQlOidcRestTemplate.exchange("",
+             val response = safGraphQlOidcRestTemplate.exchange("/",
                      HttpMethod.POST,
                      httpEntity,
                      String::class.java)
 
              if (response.statusCode.is2xxSuccessful) {
                  saf_teller_type_vellykkede.increment()
-                 return response.body!!
+                 return ResponseEntity.ok().body(response.body!!)
              } else {
                  saf_teller_type_feilede.increment()
                  throw RuntimeException("En feil oppstod under henting av dokument metadata fra SAF: ${response.statusCode}")
              }
          } catch(ex: Exception) {
              logger.error("En feil oppstod under henting av dokument metadata fra SAF: $ex")
-             return errorBody(ex.message!!, UUID.randomUUID().toString())
+
+             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                     .body(errorBody(ex.message!!, UUID.randomUUID().toString()))
          }
     }
 
-    fun hentDokumentInnhold(journalpostId: String, dokumentInfoId: String, variantFormat: String ) : String {
+    fun hentDokumentInnhold(journalpostId: String, dokumentInfoId: String, variantFormat: String ) : ResponseEntity<String> {
         try {
             val path = "/rest/hentdokument/$journalpostId/$dokumentInfoId/$variantFormat"
             val response = safRestOidcRestTemplate.exchange(path,
                     HttpMethod.GET,
-                    HttpEntity(""),
+                    HttpEntity("/"),
                     String::class.java)
             if (response.statusCode.is2xxSuccessful) {
                 saf_teller_type_vellykkede.increment()
-                return response.body!!
+                return ResponseEntity.ok().body(response.body!!)
             } else {
                 saf_teller_type_feilede.increment()
                 throw RuntimeException("En feil oppstod under henting av dokumentinnhold fra SAF: ${response.statusCode}")
             }
         } catch(ex: Exception) {
             logger.error("En feil oppstod under henting av dokumentInnhold fra SAF: $ex")
-            return errorBody(ex.message!!, UUID.randomUUID().toString())
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(errorBody(ex.message!!, UUID.randomUUID().toString()))
         }
     }
 
