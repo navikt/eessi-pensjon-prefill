51cf8b3a97da4e4dce35eca8251decde7ed03c6d
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2019-06-26 14:50:40 +0200

    Move ExperimentController to own package

--- src/main/kotlin/no/nav/eessi/eessifagmodul/experiments/ExperimentController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/experiments/ExperimentController.kt
@@ -1,5 +1,7 @@
-package no.nav.eessi.eessifagmodul.controllers
+package no.nav.eessi.eessifagmodul.experiments
 
+import no.nav.eessi.eessifagmodul.controllers.SedController
+import no.nav.eessi.eessifagmodul.json.mapAnyToJson
 import no.nav.eessi.eessifagmodul.models.IkkeGyldigKallException
 import no.nav.eessi.eessifagmodul.models.InstitusjonItem
 import no.nav.eessi.eessifagmodul.models.PersonDetail
@@ -10,7 +12,6 @@ import no.nav.eessi.eessifagmodul.services.eux.EuxService
 import no.nav.eessi.eessifagmodul.services.pensjonsinformasjon.PensjonsinformasjonService
 import no.nav.eessi.eessifagmodul.services.personv3.PersonV3Service
 import no.nav.eessi.eessifagmodul.utils.NavFodselsnummer
-import no.nav.eessi.eessifagmodul.json.mapAnyToJson
 import no.nav.pensjon.v1.pensjonsinformasjon.Pensjonsinformasjon
 import no.nav.pensjon.v1.sak.V1Sak
 import no.nav.security.oidc.api.Protected
