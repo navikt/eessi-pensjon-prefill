3e00ba31876b16fcdec2dfde218908e3ad384882
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2019-07-05 14:34:11 +0200

    Add missing import

--- src/main/kotlin/no/nav/eessi/eessifagmodul/pensjon/PensjonController.kt
+++ src/main/kotlin/no/nav/eessi/eessifagmodul/pensjon/PensjonController.kt
@@ -10,10 +10,7 @@ import no.nav.eessi.eessifagmodul.utils.mapAnyToJson
 import no.nav.security.oidc.api.Protected
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @Protected
 @RestController
