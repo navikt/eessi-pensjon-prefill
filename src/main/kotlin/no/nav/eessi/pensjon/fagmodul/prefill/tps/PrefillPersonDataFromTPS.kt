53228cb264cd182f701d5937092f8383a53e8e00
Mo Amini <mo.amini@nav.no>
2019-08-01 12:44:41 +0200

    Renset litt i Prefill TPS (#138)
    Slettet utkommentert kode
    Sletter ubrukte metoder og parametere
    Lagt på todo for metode som leverer tomme data

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/tps/PrefillPersonDataFromTPS.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/tps/PrefillPersonDataFromTPS.kt
@@ -39,18 +39,10 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
 
             return Bruker(
                     person = personData(brukerTPS),
-
                     far = hentRelasjon(RelasjonEnum.FAR, brukerTPS),
-
                     mor = hentRelasjon(RelasjonEnum.MOR, brukerTPS),
-
-//                    far = Foreldre(person = hentRelasjon(RelasjonEnum.FAR, brukerTPS)),
-//                    mor = Foreldre(person = hentRelasjon(RelasjonEnum.MOR, brukerTPS)),
-
                     adresse = hentPersonAdresse(brukerTPS),
-
                     bank = bank,
-
                     arbeidsforhold = ansettelsesforhold
             )
         } catch (ex: Exception) {
@@ -117,17 +109,12 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
                 //type
                 //5.1   -- 01 - ektefelle, 02, part i partnerskap, 3, samboer
                 type = createEktefelleType(ekteTypeValue),
-
                 //ektefelle (personobj kjører på nytt)
                 person = ektefellpartnerbruker.person,
-
                 //foreldre
                 far = ektefellpartnerbruker.far,
-
                 //foreldre
                 mor = ektefellpartnerbruker.mor
-
-
         )
     }
 
@@ -162,13 +149,6 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
         return fdato?.foedselsdato?.simpleFormat()
     }
 
-    //doddato i rina P2100?
-    private fun dodDatoFormat(person: no.nav.tjeneste.virksomhet.person.v3.informasjon.Person): String? {
-        logger.debug("4.2       Date of death / dødsdato P2100")
-        val doddato = person.doedsdato
-        return doddato?.doedsdato?.simpleFormat()
-    }
-
     fun hentFodested(bruker: no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker): Foedested? {
         logger.debug("2.1.8.1       Fødested")
 
@@ -198,17 +178,14 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
                                 PinItem(
                                         institusjonsnavn = eessiInfo.institutionnavn,
                                         institusjonsid = eessiInfo.institutionid,
-
                                         identifikator = hentNorIdent(persontps),
                                         land = "NO"
                                 )
                         ),
                         fornavn = navntps.fornavn,
                         etternavnvedfoedsel = if (RelasjonEnum.MOR.erSamme(tpsvalue)) null else navntps.etternavn
-                        //doedsdato = dodDatoFormat(persontps)
                 )
                 return Foreldre(person = relasjonperson)
-                //return relasjonperson
             }
         }
         return null
@@ -222,35 +199,25 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
         val kjonn = brukerTps.kjoenn
 
         return Person(
-
                 //2.1.1     familiy name
                 etternavn = navn.etternavn,
-
                 //2.1.2     forname
                 fornavn = navn.fornavn,
-
                 //2.1.3
                 foedselsdato = datoFormat(brukerTps),
-
                 //2.1.4     //sex
                 kjoenn = mapKjonn(kjonn),
-
                 //2.1.6
                 fornavnvedfoedsel = navn.fornavn,
-
                 //2.1.7
                 pin = hentPersonPinNorIdent(brukerTps),
-
                 //2.2.1.1
                 statsborgerskap = listOf(hentStatsborgerskapTps(brukerTps)),
-
                 //2.1.8.1           place of birth
                 foedested = hentFodested(brukerTps),
-
                 //2.2.2
                 sivilstand = hentSivilstand(brukerTps)
         )
-
     }
 
     private fun hentPersonPinNorIdent(brukerTps: no.nav.tjeneste.virksomhet.person.v3.informasjon.Bruker): List<PinItem> {
@@ -262,7 +229,6 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
                         institusjonsid = eessiInfo.institutionid,
 
                         //NAV/Norge benytter ikke seg av sektor, setter denne til null
-                        //sektor = "",
 
                         //personnr
                         identifikator = hentNorIdent(brukerTps),
@@ -283,11 +249,9 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
         logger.debug("2.2.2           Sivilstand / Familiestatus (01 Enslig, 02 Gift, 03 Samboer, 04 Partnerskal, 05 Skilt, 06 Skilt partner, 07 Separert, 08 Enke)")
         val sivilstand = brukerTps.sivilstand as Sivilstand
 
-        //val status = mapOf<String, String>("01" to "UGIF", "02" to "GIFT", "03" to "SAMB", "04" to "REPA", "05" to "SKIL", "06" to "SKPA", "07" to "SEPA", "08" to "ENKE")
         val status = mapOf("GIFT" to "02", "REPA" to "04", "ENKE" to "08", "SAMB" to "03", "SEPA" to "07", "UGIF" to "01", "SKIL" to "05", "SKPA" to "06")
 
         return listOf(SivilstandItem(
-                //fradato = standardDatoformat(sivilstand.fomGyldighetsperiode),
                 fradato = sivilstand.fomGyldighetsperiode.simpleFormat(),
                 status = status[sivilstand.sivilstand.value]
         ))
@@ -306,7 +270,7 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
         }
 
         //Gateadresse eller UstrukturertAdresse
-        val bostedsadresse: Bostedsadresse = personTPS.bostedsadresse ?: return hentPersonAdresseUstrukturert(personTPS.postadresse)
+        val bostedsadresse: Bostedsadresse = personTPS.bostedsadresse ?: return hentPersonAdresseUstrukturert()
 
         val gateAdresse = bostedsadresse.strukturertAdresse as Gateadresse
         val gate = gateAdresse.gatenavn
@@ -319,13 +283,15 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
         )
     }
 
-    //2.2.2 ustrukturert
-    private fun hentPersonAdresseUstrukturert(postadr: Postadresse): Adresse {
+    //TODO: Denne metoden gjør ikke det den sier at den skal gjøre
+    /**
+     *  2.2.2 ustrukturert
+     *
+     *  Returnerer en bank adresse dersom det finnes en ustrukturertAdresse hos borger.
+     *  Dette må så endres/rettes av saksbehendlaer i rina?
+     */
+    private fun hentPersonAdresseUstrukturert(): Adresse {
         logger.debug("             UstrukturertAdresse (utland)")
-        val gateAdresse = postadr.ustrukturertAdresse as UstrukturertAdresse
-
-        //returnerer nå en bank adresse dersom det finnes en ustrukturertAdresse hos borger.
-        //dette må så endres/rettes av saksbehendlaer i rina?
         return Adresse(
                 gate = "",
                 bygning = "",
@@ -333,20 +299,6 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
                 postnummer = "",
                 land = ""
         )
-
-//        try {
-//
-//                    gate = gateAdresse.adresselinje1 ?: null,
-//                    bygning = gateAdresse?.adresselinje2 ?: null,
-//                    by = gateAdresse?.adresselinje3 ?: "",
-//                    postnummer = gateAdresse?.adresselinje4 ?: null,
-//                    land = hentLandkode(gateAdresse.landkode)
-//            )
-//        } catch (ex: Exception) {
-//            logger.error("Feiler ved mapping av Ustrukturert Adresse.")
-//            return Adresse()
-//        }
-
     }
 
     //knytes til nasjonalitet for utfylling P2x00
@@ -370,11 +322,6 @@ class PrefillPersonDataFromTPS(private val personV3Service: PersonV3Service,
     private fun mapKjonn(kjonn: Kjoenn): String {
         logger.debug("2.1.4         Kjønn")
         val ktyper = kjonn.kjoenn
-//        val map: Map<String, String> = hashMapOf("M" to "m", "K" to "f")
-//        val value = map[ktyper.value]
-//        return value ?: "u"
         return ktyper.value
     }
-
-
 }
