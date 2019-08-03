navikt/eessi-pensjon-prefill

commit a8bfbbaed2dfec98bf88b7004372bdd86b7a7021

Fjernet 3 dupliserte metoder fra (#135)
Prefill2000
Prefill2200
Prefill6000

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/PrefillFactory.kt
@@ -3,7 +3,6 @@ package no.nav.eessi.pensjon.fagmodul.prefill.sed
 import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
 import no.nav.eessi.pensjon.fagmodul.models.SEDType
 import no.nav.eessi.pensjon.fagmodul.prefill.eessi.EessiInformasjon
-import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
 import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.PrefillP2000
@@ -11,6 +10,7 @@ import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.PrefillP2200
 import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
 import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillPensjon
 import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillPerson
+import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.KravDataFromPEN
 import no.nav.eessi.pensjon.fagmodul.prefill.tps.PrefillPersonDataFromTPS
 import no.nav.eessi.pensjon.fagmodul.prefill.sed.vedtak.PrefillP6000
 import org.slf4j.Logger
@@ -19,9 +19,9 @@ import org.springframework.stereotype.Component
 
 @Component
 class PrefillFactory(private val prefillNav: PrefillNav,
-                     private val dataFromTPS: PrefillPersonDataFromTPS,
-                     private val dataFromPEN: PensjonsinformasjonHjelper,
-                     private val eessiInformasjon: EessiInformasjon) {
+                     dataFromTPS: PrefillPersonDataFromTPS,
+                     private val eessiInformasjon: EessiInformasjon,
+                     private val sakPensiondata: KravDataFromPEN) {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillFactory::class.java) }
 
@@ -35,16 +35,16 @@ class PrefillFactory(private val prefillNav: PrefillNav,
         return when (sedValue) {
             //Status hva gjendstår
             SEDType.P6000 -> {
-                PrefillP6000(prefillNav, dataFromTPS, eessiInformasjon, dataFromPEN)
+                PrefillP6000(sakPensiondata, eessiInformasjon)
             }
             //Status hva gjendstår
             SEDType.P2000 -> {
                 //PrefillDefaultSED(prefillPerson)
-                PrefillP2000(prefillNav, dataFromTPS, dataFromPEN)
+                PrefillP2000(sakPensiondata)
             }
             //Status hva gjendstår
             SEDType.P2200 -> {
-                PrefillP2200(prefillNav, dataFromTPS, dataFromPEN)
+                PrefillP2200(sakPensiondata)
             }
             SEDType.P2100 -> {
                 PrefillP2100(prefillPerson)
@@ -70,7 +70,6 @@ class PrefillFactory(private val prefillNav: PrefillNav,
                 //P5000, P8000, P7000, P8000  - P9000 og P10000
                 PrefillDefaultSED(prefillPerson)
             }
-
         }
     }
 }

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/PrefillP6000.kt
@@ -1,34 +1,23 @@
 package no.nav.eessi.pensjon.fagmodul.prefill.sed.vedtak
 
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Nav
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Bruker
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Pensjon
-import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
 import no.nav.eessi.pensjon.fagmodul.prefill.eessi.EessiInformasjon
-import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
+import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
-import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
-import no.nav.eessi.pensjon.fagmodul.prefill.tps.PrefillPersonDataFromTPS
+import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.KravDataFromPEN
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
-class PrefillP6000(private val prefillNav: PrefillNav,
-                   private val dataFromTPS: PrefillPersonDataFromTPS,
-                   private val eessiInfo: EessiInformasjon,
-                   penHjelper: PensjonsinformasjonHjelper) : Prefill<SED> {
+class PrefillP6000(private val sakPensiondata: KravDataFromPEN,
+                   private val eessiInfo: EessiInformasjon) : Prefill<SED> {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillP6000::class.java) }
-
-    private var vedtakPensiondata: VedtakDataFromPEN = VedtakDataFromPEN(penHjelper)
 
     override fun prefill(prefillData: PrefillDataModel): SED {
         val sedId = prefillData.getSEDid()
 
         logger.debug("----------------------------------------------------------"
-                + "\nPreutfylling NAV     : ${prefillNav::class.java} "
-                + "\nPreutfylling TPS     : ${dataFromTPS::class.java} "
-                + "\nPreutfylling Pensjon : ${vedtakPensiondata::class.java} "
+                + "\nPreutfylling Pensjon : ${sakPensiondata::class.java} "
                 + "\n------------------| Preutfylling [$sedId] START |------------------ ")
 
         val sed = prefillData.sed
@@ -38,43 +27,17 @@ class PrefillP6000(private val prefillNav: PrefillNav,
         logger.info("Andreinstitusjoner: ${prefillData.andreInstitusjon} ")
 
         logger.debug("Henter opp Pernsjondata fra PESYS")
-        val pensjon = createPensjon(prefillData)
+        val pensjon = sakPensiondata.createPensjon(prefillData)
         sed.pensjon = pensjon
 
         logger.debug("Henter opp Persondata fra TPS")
-        sed.nav = createNav(prefillData)
+        sed.nav = sakPensiondata.createNav(prefillData)
 
         logger.debug("Henter opp Persondata/Gjenlevende fra TPS")
-        pensjon.gjenlevende = createGjenlevende(prefillData)
+        pensjon.gjenlevende = sakPensiondata.createGjenlevende(prefillData)
 
         logger.debug("-------------------| Preutfylling [$sedId] END |------------------- ")
         return prefillData.sed
     }
-
-    //henter persondata fra TPS fyller ut sed.nav
-    private fun createNav(prefillData: PrefillDataModel): Nav {
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling Utfylling NAV")
-        return prefillNav.prefill(prefillData)
-    }
-
-    //henter pensjondata fra PESYS fyller ut sed.pensjon
-    private fun createPensjon(prefillData: PrefillDataModel): Pensjon {
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling Utfylling Pensjon")
-        return vedtakPensiondata.prefill(prefillData)
-    }
-
-    //fylles ut kun når vi har etterlatt aktoerId og etterlattPinID.
-    //noe vi må få fra PSAK. o.l
-    private fun createGjenlevende(prefillData: PrefillDataModel): Bruker? {
-        var gjenlevende: Bruker? = null
-        if (prefillData.erGyldigEtterlatt()) {
-            logger.debug("Preutfylling Utfylling Pensjon Gjenlevende (etterlatt)")
-            gjenlevende = dataFromTPS.prefillBruker(prefillData.personNr)
-        }
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling Utfylling Pensjon TPS")
-        return gjenlevende
-    }
-
-
 }
 

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/krav/KravDataFromPEN.kt
@@ -1,15 +1,12 @@
 package no.nav.eessi.pensjon.fagmodul.prefill.sed.krav
 
-import no.nav.eessi.pensjon.fagmodul.sedmodel.BeloepItem
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Institusjon
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Krav
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Pensjon
-import no.nav.eessi.pensjon.fagmodul.sedmodel.PinItem
-import no.nav.eessi.pensjon.fagmodul.sedmodel.YtelserItem
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
 import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
+import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
 import no.nav.eessi.pensjon.fagmodul.prefill.tps.NavFodselsnummer
+import no.nav.eessi.pensjon.fagmodul.prefill.tps.PrefillPersonDataFromTPS
+import no.nav.eessi.pensjon.fagmodul.sedmodel.*
 import no.nav.eessi.pensjon.utils.simpleFormat
 import no.nav.pensjon.v1.kravhistorikk.V1KravHistorikk
 import no.nav.pensjon.v1.pensjonsinformasjon.Pensjonsinformasjon
@@ -18,11 +15,15 @@ import no.nav.pensjon.v1.ytelsepermaaned.V1YtelsePerMaaned
 import no.nav.pensjon.v1.ytelseskomponent.V1Ytelseskomponent
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Component
 
 /**
  * Hjelpe klasse for sak som fyller ut NAV-SED-P2000 med pensjondata fra PESYS.
  */
-open class KravDataFromPEN(private val dataFromPEN: PensjonsinformasjonHjelper) : Prefill<Pensjon> {
+@Component
+class KravDataFromPEN(private val prefillNav: PrefillNav,
+                      private val preutfyllingPersonFraTPS: PrefillPersonDataFromTPS,
+                      private val dataFromPEN: PensjonsinformasjonHjelper) : Prefill<Pensjon> {
     private val logger: Logger by lazy { LoggerFactory.getLogger(KravDataFromPEN::class.java) }
 
     //gyldige kravhistorikk status og typer.
@@ -532,5 +533,34 @@ open class KravDataFromPEN(private val dataFromPEN: PensjonsinformasjonHjelper) 
         }
         logger.error("Fant ikke noe Kravhistorikk..$TIL_BEHANDLING HVA GJØR VI NÅ?")
         return V1KravHistorikk()
+    }
+
+    /**
+     *  Henter persondata fra TPS fyller ut sed.nav
+     */
+    fun createNav(prefillData: PrefillDataModel): Nav {
+        logger.debug("[${prefillData.getSEDid()}] Preutfylling NAV")
+        return prefillNav.prefill(prefillData)
+    }
+
+    /**
+     *  Henter pensjondata fra PESYS fyller ut sed.pensjon
+     */
+    fun createPensjon(prefillData: PrefillDataModel): Pensjon {
+        logger.debug("[${prefillData.getSEDid()}]   Preutfylling PENSJON")
+        return prefill(prefillData)
+    }
+
+    /**
+     *  fylles ut kun når vi har etterlatt etterlattPinID.
+     *  noe vi må få fra PSAK. o.l
+     */
+    fun createGjenlevende(prefillData: PrefillDataModel): Bruker? {
+        var gjenlevende: Bruker? = null
+        if (prefillData.erGyldigEtterlatt()) {
+            logger.debug("          Utfylling gjenlevende (etterlatt)")
+            gjenlevende = preutfyllingPersonFraTPS.prefillBruker(prefillData.personNr)
+        }
+        return gjenlevende
     }
 }

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/krav/PrefillP2000.kt
@@ -2,14 +2,10 @@ package no.nav.eessi.pensjon.fagmodul.prefill.sed.krav
 
 import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
 import no.nav.eessi.pensjon.fagmodul.sedmodel.Nav
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Bruker
 import no.nav.eessi.pensjon.fagmodul.sedmodel.Pensjon
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
 import no.nav.eessi.pensjon.fagmodul.prefill.model.ValidationException
-import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
-import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
-import no.nav.eessi.pensjon.fagmodul.prefill.tps.PrefillPersonDataFromTPS
 import no.nav.eessi.pensjon.services.pensjonsinformasjon.PensjoninformasjonException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -17,11 +13,10 @@ import org.slf4j.LoggerFactory
 /**
  * preutfylling av NAV-P2000 SED for søknad krav om alderpensjon
  */
-class PrefillP2000(private val prefillNav: PrefillNav, private val preutfyllingPersonFraTPS: PrefillPersonDataFromTPS, dataFromPEN: PensjonsinformasjonHjelper) : Prefill<SED> {
+class PrefillP2000(private val sakPensiondata: KravDataFromPEN) : Prefill<SED> {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillP2000::class.java) }
 
-    private val sakPensiondata: KravDataFromPEN = KravDataFromPEN(dataFromPEN)
 
     override fun prefill(prefillData: PrefillDataModel): SED {
         val sedId = prefillData.getSEDid()
@@ -30,8 +25,6 @@ class PrefillP2000(private val prefillNav: PrefillNav, private val preutfyllingP
         logger.debug("----------------------------------------------------------"
                 + "\nSaktype              : ${prefillData.saktype} "
                 + "\nSøker etter SaktId   : ${prefillData.penSaksnummer} "
-                + "\nPreutfylling NAV     : ${prefillNav::class.java} "
-                + "\nPreutfylling TPS     : ${preutfyllingPersonFraTPS::class.java} "
                 + "\nPreutfylling Pensjon : ${sakPensiondata::class.java} "
                 + "\n------------------| Preutfylling [$sedId] START |------------------ ")
 
@@ -42,23 +35,23 @@ class PrefillP2000(private val prefillNav: PrefillNav, private val preutfyllingP
             sed.nav = Nav()
             //henter opp persondata
         } else {
-            sed.nav = createNav(prefillData)
+            sed.nav = sakPensiondata.createNav(prefillData)
         }
 
         //skipper å henter opp pensjondata hvis PENSED finnes
         try {
             if (prefillData.kanFeltSkippes("PENSED")) {
-                val pensjon = createPensjon(prefillData)
+                val pensjon = sakPensiondata.createPensjon(prefillData)
                 //vi skal ha blank pensjon ved denne toggle
                 //vi må ha med kravdato
                 sed.pensjon = Pensjon(kravDato = pensjon.kravDato)
 
                 //henter opp pensjondata
             } else {
-                val pensjon = createPensjon(prefillData)
+                val pensjon = sakPensiondata.createPensjon(prefillData)
 
                 //gjenlevende hvis det finnes..
-                pensjon.gjenlevende = createGjenlevende(prefillData)
+                pensjon.gjenlevende = sakPensiondata.createGjenlevende(prefillData)
                 //legger pensjon på sed (få med oss gjenlevende/avdød)
                 sed.pensjon = pensjon
             }
@@ -81,29 +74,6 @@ class PrefillP2000(private val prefillNav: PrefillNav, private val preutfyllingP
 
         logger.debug("-------------------| Preutfylling [$sedId] END |------------------- ")
         return prefillData.sed
-    }
-
-    //henter persondata fra TPS fyller ut sed.nav
-    private fun createNav(prefillData: PrefillDataModel): Nav {
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling NAV")
-        return prefillNav.prefill(prefillData)
-    }
-
-    //henter pensjondata fra PESYS fyller ut sed.pensjon
-    private fun createPensjon(prefillData: PrefillDataModel): Pensjon {
-        logger.debug("[${prefillData.getSEDid()}]   Preutfylling PENSJON")
-        return sakPensiondata.prefill(prefillData)
-    }
-
-    //fylles ut kun når vi har etterlatt etterlattPinID.
-    //noe vi må få fra PSAK. o.l
-    private fun createGjenlevende(prefillData: PrefillDataModel): Bruker? {
-        var gjenlevende: Bruker? = null
-        if (prefillData.erGyldigEtterlatt()) {
-            logger.debug("          Utfylling gjenlevende (etterlatt)")
-            gjenlevende = preutfyllingPersonFraTPS.prefillBruker(prefillData.personNr)
-        }
-        return gjenlevende
     }
 
     override fun validate(data: SED) {

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/krav/PrefillP2200.kt
@@ -1,14 +1,9 @@
 package no.nav.eessi.pensjon.fagmodul.prefill.sed.krav
 
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Nav
-import no.nav.eessi.pensjon.fagmodul.sedmodel.Bruker
 import no.nav.eessi.pensjon.fagmodul.sedmodel.Pensjon
 import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
-import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
-import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
-import no.nav.eessi.pensjon.fagmodul.prefill.tps.PrefillPersonDataFromTPS
 import no.nav.eessi.pensjon.services.pensjonsinformasjon.PensjoninformasjonException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -17,42 +12,38 @@ import org.slf4j.LoggerFactory
 /**
  * preutfylling av NAV-P2200 SED for søknad krav om uforepensjon
  */
-class PrefillP2200(private val prefillNav: PrefillNav, private val preutfyllingPersonFraTPS: PrefillPersonDataFromTPS, pensjonHjelper: PensjonsinformasjonHjelper) : Prefill<SED> {
+class PrefillP2200(private val sakPensiondata: KravDataFromPEN) : Prefill<SED> {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillP2200::class.java) }
-
-    private val sakPensiondata: KravDataFromPEN = KravDataFromPEN(pensjonHjelper)
 
     override fun prefill(prefillData: PrefillDataModel): SED {
         val sedId = prefillData.getSEDid()
 
         logger.debug("----------------------------------------------------------"
-                + "\nPreutfylling NAV     : ${prefillNav::class.java} "
-                + "\nPreutfylling TPS     : ${preutfyllingPersonFraTPS::class.java} "
                 + "\nPreutfylling Pensjon : ${sakPensiondata::class.java} "
                 + "\n------------------| Preutfylling [$sedId] START |------------------ ")
 
         val sed = prefillData.sed
 
         //henter opp persondata
-        sed.nav = createNav(prefillData)
+        sed.nav = sakPensiondata.createNav(prefillData)
 
         //henter opp pensjondat
         //val pensjon = createPensjon(prefillData)
         //skipper å henter opp pensjondata hvis PENSED finnes
         try {
             if (prefillData.kanFeltSkippes("PENSED")) {
-                val pensjon = createPensjon(prefillData)
+                val pensjon = sakPensiondata.createPensjon(prefillData)
                 //vi skal ha blank pensjon ved denne toggle
                 //vi må ha med kravdato
                 sed.pensjon = Pensjon(kravDato = pensjon.kravDato)
 
                 //henter opp pensjondata
             } else {
-                val pensjon = createPensjon(prefillData)
+                val pensjon = sakPensiondata.createPensjon(prefillData)
 
                 //gjenlevende hvis det finnes..
-                pensjon.gjenlevende = createGjenlevende(prefillData)
+                pensjon.gjenlevende = sakPensiondata.createGjenlevende(prefillData)
                 //legger pensjon på sed (få med oss gjenlevende/avdød)
                 sed.pensjon = pensjon
             }
@@ -73,32 +64,7 @@ class PrefillP2200(private val prefillNav: PrefillNav, private val preutfyllingP
             sed.nav?.krav = sed.pensjon?.kravDato
         }
 
-
         logger.debug("-------------------| Preutfylling [$sedId] END |------------------- ")
         return prefillData.sed
     }
-
-    //henter persondata fra TPS fyller ut sed.nav
-    private fun createNav(prefillData: PrefillDataModel): Nav {
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling Utfylling NAV")
-        return prefillNav.prefill(prefillData)
-    }
-
-    //henter pensjondata fra PESYS fyller ut sed.pensjon
-    private fun createPensjon(prefillData: PrefillDataModel): Pensjon {
-        logger.debug("[${prefillData.getSEDid()}] Preutfylling Utfylling Pensjon")
-        return sakPensiondata.prefill(prefillData)
-    }
-
-    //fylles ut kun når vi har etterlatt etterlattPinID.
-    //noe vi må få fra PSAK. o.l
-    private fun createGjenlevende(prefillData: PrefillDataModel): Bruker? {
-        var gjenlevende: Bruker? = null
-        if (prefillData.erGyldigEtterlatt()) {
-            logger.debug("          Utfylling gjenlevende (etterlatt)")
-            gjenlevende = preutfyllingPersonFraTPS.prefillBruker(prefillData.personNr)
-        }
-        return gjenlevende
-    }
-
 }

src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/AbstractPrefillIntegrationTestHelper.kt
@@ -166,8 +166,8 @@ abstract class AbstractPrefillIntegrationTestHelper {
         return PensjonsinformasjonHjelper(pensjonsinformasjonService1)
     }
 
-    private fun mockKravDataFromPEN(prefillPensionDataFromPEN: PensjonsinformasjonHjelper): KravDataFromPEN {
-        return KravDataFromPEN(prefillPensionDataFromPEN)
+    fun mockKravDataFromPEN(prefillPensionDataFromPEN: PensjonsinformasjonHjelper): KravDataFromPEN {
+        return KravDataFromPEN(prefillNav, personTPS, prefillPensionDataFromPEN)
     }
 
 

src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/krav/PrefillP2000-AP-21975717Test.kt
@@ -27,7 +27,7 @@ class `PrefillP2000-AP-21975717Test` : AbstractPrefillIntegrationTestHelper() {
     }
 
     override fun createTestClass(prefillNav: PrefillNav, personTPS: PrefillPersonDataFromTPS, pensionDataFromPEN: PensjonsinformasjonHjelper): Prefill<SED> {
-        return PrefillP2000(prefillNav, personTPS, pensionDataFromPEN)
+        return PrefillP2000(kravdata)
     }
 
     override fun createSaksnummer(): String {

src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/krav/PrefillP2000-AP-LOP-REVUTest.kt
@@ -24,7 +24,7 @@ class `PrefillP2000-AP-LOP-REVUTest` : AbstractPrefillIntegrationTestHelper() {
     }
 
     override fun createTestClass(prefillNav: PrefillNav, personTPS: PrefillPersonDataFromTPS, pensionDataFromPEN: PensjonsinformasjonHjelper): Prefill<SED> {
-        return PrefillP2000(prefillNav, personTPS, pensionDataFromPEN)
+        return PrefillP2000(kravdata)
     }
 
     override fun createPayload(prefillData: PrefillDataModel) {

commit bf78ca5b26537b92e1b8282c682449570396a3a2

Fix bug introduced in a8bfbba

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/PrefillFactory.kt
@@ -5,6 +5,7 @@ import no.nav.eessi.pensjon.fagmodul.models.SEDType
 import no.nav.eessi.pensjon.fagmodul.prefill.eessi.EessiInformasjon
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
+import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
 import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.PrefillP2000
 import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.PrefillP2200
 import no.nav.eessi.pensjon.fagmodul.prefill.person.PrefillNav
@@ -21,7 +22,8 @@ import org.springframework.stereotype.Component
 class PrefillFactory(private val prefillNav: PrefillNav,
                      dataFromTPS: PrefillPersonDataFromTPS,
                      private val eessiInformasjon: EessiInformasjon,
-                     private val sakHelper: SakHelper) {
+                     private val sakHelper: SakHelper,
+                     private val dataFromPEN: PensjonsinformasjonHjelper) {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillFactory::class.java) }
 
@@ -35,7 +37,7 @@ class PrefillFactory(private val prefillNav: PrefillNav,
         return when (sedValue) {
             //Status hva gjendstår
             SEDType.P6000 -> {
-                PrefillP6000(sakHelper, eessiInformasjon)
+                PrefillP6000(sakHelper, eessiInformasjon, dataFromPEN)
             }
             //Status hva gjendstår
             SEDType.P2000 -> {

src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/PrefillP6000.kt
@@ -4,14 +4,18 @@ import no.nav.eessi.pensjon.fagmodul.prefill.eessi.EessiInformasjon
 import no.nav.eessi.pensjon.fagmodul.sedmodel.SED
 import no.nav.eessi.pensjon.fagmodul.prefill.model.Prefill
 import no.nav.eessi.pensjon.fagmodul.prefill.model.PrefillDataModel
+import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
 import no.nav.eessi.pensjon.fagmodul.prefill.sed.krav.SakHelper
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class PrefillP6000(private val sakHelper: SakHelper,
-                   private val eessiInfo: EessiInformasjon) : Prefill<SED> {
+                   private val eessiInfo: EessiInformasjon,
+                   dataFromPESYS: PensjonsinformasjonHjelper) : Prefill<SED> {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillP6000::class.java) }
+
+    private var vedtakPensiondata: VedtakDataFromPEN = VedtakDataFromPEN(dataFromPESYS)
 
     override fun prefill(prefillData: PrefillDataModel): SED {
         val sedId = prefillData.getSEDid()
@@ -30,7 +34,7 @@ class PrefillP6000(private val sakHelper: SakHelper,
         val gjenlevende = sakHelper.createGjenlevende(prefillData)
 
         logger.debug("Henter opp Pernsjondata fra PESYS")
-        val pensjon = sakHelper.createPensjon(prefillData, gjenlevende)
+        val pensjon = vedtakPensiondata.prefill(prefillData)
         sed.pensjon = pensjon
 
         logger.debug("Henter opp Persondata fra TPS")