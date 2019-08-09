1f8cc9a4f1fc5ff1315036e38664744c837a58ff
Jens Madsen <jens.christian.madsen@nav.no>
2019-08-09 14:05:35 +0200

    Må ha med korrekt testdata når parsing av dato blitt endret

--- src/main/kotlin/no/nav/eessi/pensjon/utils/DateUtils.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/utils/DateUtils.kt
@@ -2,22 +2,33 @@ package no.nav.eessi.pensjon.utils
 
 import java.text.SimpleDateFormat
 import java.time.LocalDate
+import java.time.LocalDateTime
 import java.time.ZoneId
+import java.time.format.DateTimeFormatter
 import java.util.*
 import javax.xml.datatype.DatatypeFactory
 import javax.xml.datatype.XMLGregorianCalendar
 
-fun XMLGregorianCalendar.simpleFormat() =
-    if (this.year > 2500) "" else SimpleDateFormat("yyyy-MM-dd").format(this.toGregorianCalendar().time)
+fun XMLGregorianCalendar.simpleFormat(): String {
+    if (this.year > 2500) {
+        return ""
+    }
+    println("XMLGregorianCalendar: date: ${this.toString()}")
+    val date = SimpleDateFormat("yyyy-MM-dd").parse(this.toString())
+    println("Date: ${date.toString()}")
+    return SimpleDateFormat("yyyy-MM-dd").format(date)
+}
 
-
-fun Date.simpleFormat() =
-        SimpleDateFormat("yyyy-MM-dd").format(this)
+fun Date.simpleFormat() = SimpleDateFormat("yyyy-MM-dd").format(this)
 
 fun createXMLCalendarFromString(dateStr: String): XMLGregorianCalendar {
-    val time = LocalDate.parse(dateStr)
+    println(dateStr)
+    val date = SimpleDateFormat("yyyy-MM-dd").parse(dateStr)
+    println(date)
+    //val time = LocalDate.parse(dateStr)
     val gcal = GregorianCalendar()
-    gcal.timeInMillis = time.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli()
+    //gcal.timeInMillis = time.atStartOfDay (ZoneId.systemDefault()).toInstant().toEpochMilli()
+     gcal.timeInMillis = date.time
     return DatatypeFactory.newInstance().newXMLGregorianCalendar(gcal)
 }
 
