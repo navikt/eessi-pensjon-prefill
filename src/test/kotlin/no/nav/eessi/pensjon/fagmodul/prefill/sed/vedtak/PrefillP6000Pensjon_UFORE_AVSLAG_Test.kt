62cf007d75ab3e2aad3c5f29c3a8d5c185a5bfcb
Jens Madsen <jens.christian.madsen@nav.no>
2019-08-26 16:47:15 +0200

    oppdatert prefillP6000 med fiks på mulige avslag som er tom, lite data

---
+++ src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/PrefillP6000Pensjon_UFORE_AVSLAG_Test.kt
@@ -0,0 +1,68 @@
+package no.nav.eessi.pensjon.fagmodul.prefill.sed.vedtak
+
+import no.nav.eessi.pensjon.fagmodul.prefill.eessi.EessiInformasjonMother.standardEessiInfo
+import no.nav.eessi.pensjon.fagmodul.prefill.pen.PensjonsinformasjonHjelper
+import no.nav.eessi.pensjon.services.pensjonsinformasjon.PensjonsinformasjonServiceMother.fraFil
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.junit.jupiter.MockitoSettings
+import org.mockito.quality.Strictness
+
+@ExtendWith(MockitoExtension::class)
+@MockitoSettings(strictness = Strictness.LENIENT)
+class PrefillP6000Pensjon_UFORE_AVSLAG_Test {
+
+    @Test
+    fun `forventet korrekt utfylling av Pensjon objekt på Uførepensjon med avslag`() {
+        val dataFromPESYS = PensjonsinformasjonHjelper(fraFil("P6000-UF-Avslag.xml"))
+
+        val result = PrefillP6000Pensjon.createPensjon(
+                dataFromPESYS = dataFromPESYS,
+                gjenlevende = null,
+                vedtakId = "12312312",
+                andreinstitusjonerItem = standardEessiInfo().asAndreinstitusjonerItem())
+
+        assertNotNull(result.vedtak)
+        assertNotNull(result.sak)
+        assertNotNull(result.tilleggsinformasjon)
+
+        val vedtak = result.vedtak?.get(0)
+        assertEquals("2019-08-01", vedtak?.virkningsdato, "vedtak.virkningsdato")
+        assertEquals("02", vedtak?.type)
+        assertEquals("99", vedtak?.basertPaa)
+        assertEquals("01", vedtak?.resultat, "vedtak.resultat")
+        assertEquals("2019-08-26", vedtak?.kjoeringsdato)
+        assertEquals(null, vedtak?.artikkel, "4.1.5 vedtak.artikkel (må fylles ut manuelt nå)")
+
+        assertEquals(null, vedtak?.grunnlag?.opptjening?.forsikredeAnnen)
+        assertEquals(null, vedtak?.grunnlag?.framtidigtrygdetid)
+
+        assertEquals(null, vedtak?.ukjent?.beloepBrutto?.ytelseskomponentAnnen)
+
+        val avslagBegrunnelse = vedtak?.avslagbegrunnelse?.get(0)
+        assertEquals(null, avslagBegrunnelse?.begrunnelse)
+
+        assertEquals("six weeks from the date the decision is received", result.sak?.kravtype?.get(0)?.datoFrist)
+
+        assertEquals("2019-08-26", result.tilleggsinformasjon?.dato)
+
+        assertEquals("NO:noinst002", result.tilleggsinformasjon?.andreinstitusjoner?.get(0)?.institusjonsid)
+        assertEquals("NOINST002, NO INST002, NO", result.tilleggsinformasjon?.andreinstitusjoner?.get(0)?.institusjonsnavn)
+        assertEquals("Postboks 6600 Etterstad TEST", result.tilleggsinformasjon?.andreinstitusjoner?.get(0)?.institusjonsadresse)
+        assertEquals("0607", result.tilleggsinformasjon?.andreinstitusjoner?.get(0)?.postnummer)
+
+    }
+
+    @Test
+    fun `preutfylling P6000 feiler ved mangler av vedtakId`() {
+        val dataFromPESYS = PensjonsinformasjonHjelper(fraFil("P6000-UF-Avslag.xml"))
+
+        assertThrows<IllegalStateException> {
+            PrefillP6000Pensjon.createPensjon(dataFromPESYS, null, "", null)
+        }
+    }
+}
