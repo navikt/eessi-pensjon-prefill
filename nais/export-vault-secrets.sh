68d8e06a5d47c38a7e45414b1fd5d694768d44ba
moamini <mo.amini@nav.no>
2020-01-31 08:16:22 +0100

    Oppdatert username

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -4,12 +4,12 @@ echo "Sjekker eessi-pensjon-fagmodul srvPassord"
 if test -f /var/run/secrets/nais.io/srveessipensjon/password;
 then
   echo "Setter eessi-pensjon-fagmodul srvPassord"
-    export password=$(cat /var/run/secrets/nais.io/srveessipensjon/password)
+    export srvpassword=$(cat /var/run/secrets/nais.io/srveessipensjon/password)
 fi
 
 echo "Sjekker eessi-pensjon-fagmodul srvUsername"
 if test -f /var/run/secrets/nais.io/srveessipensjon/username;
 then
     echo "Setter eessi-pensjon-fagmodul srvUsername"
-    export username=$(cat /var/run/secrets/nais.io/srveessipensjon/username)
+    export srvusername=$(cat /var/run/secrets/nais.io/srveessipensjon/username)
 fi
