9f8f4e04783bf0fd7730fc83d93757ad13d8c3bc
moamini <mo.amini@nav.no>
2020-04-17 14:43:25 +0200

    Fjernet unødvendig application main

--- src/test/kotlin/no/nav/eessi/pensjon/UnsecuredWebMvcTestLauncher.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/UnsecuredWebMvcTestLauncher.kt
@@ -1,14 +1,17 @@
 package no.nav.eessi.pensjon
 
+import no.nav.security.spring.oidc.api.EnableOIDCTokenValidation
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Profile
 import org.springframework.test.annotation.DirtiesContext
 
+@EnableOIDCTokenValidation(ignore = ["org.springframework", "springfox.documentation", "no.nav.eessi"])
 @SpringBootApplication
 @Profile("unsecured-webmvctest")
 @DirtiesContext
-class UnsecuredWebMvcTestLauncher
+class UnsecuredWebMvcTestLauncher : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<UnsecuredWebMvcTestLauncher>(*args)
