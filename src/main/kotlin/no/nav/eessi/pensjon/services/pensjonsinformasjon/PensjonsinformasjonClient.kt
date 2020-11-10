0bdf56815ab872cd5865d5cc263b269501a65c5f
Jens Chr. Madsen <jens.christian.madsen@nav.no>
2020-11-10 14:44:18 +0100

    logger bodd arbeidet utlandet xml for prodfeil rettet feil i test
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/services/pensjonsinformasjon/PensjonsinformasjonClient.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/services/pensjonsinformasjon/PensjonsinformasjonClient.kt
@@ -116,21 +116,27 @@ class PensjonsinformasjonClient(
             logger.debug("Pensjonsinformasjon xml: $xmlString")
             val res = unmarshaller.unmarshal(StreamSource(StringReader(xmlString)), Pensjonsinformasjon::class.java)
 
-            res.value as Pensjonsinformasjon
-            val peninfo = res.value
-
-//            21434854
-            val saknr =  21434854L
-            if (peninfo.brukersSakerListe.brukersSakerListe.firstOrNull { it.sakId == saknr } != null ) {
-                logger.info("**************Pensjonsinformasjon xml: $xmlString  \n\n**************")
-            }
-
-            return peninfo
+            return printLogg(xmlString, res.value as Pensjonsinformasjon)
 
         } catch (ex: Exception) {
             logger.error("Feiler med xml transformering til Pensjoninformasjon")
             throw PensjoninformasjonProcessingException("Feiler med xml transformering til Pensjoninformasjon: ${ex.message}")
         }
+    }
+
+    fun printLogg(xmlString: String, pensjonsinformasjon: Pensjonsinformasjon): Pensjonsinformasjon {
+        val peninfo = pensjonsinformasjon
+
+        try {
+            val saknr =  21434854L
+            if (peninfo.brukersSakerListe.brukersSakerListe.firstOrNull { it.sakId == saknr } != null ) {
+                logger.info("**************Pensjonsinformasjon xml: $xmlString  \n\n**************")
+            }
+        } catch (ex: Exception) {
+            logger.warn("Ingen sak funnet.")
+        }
+
+        return pensjonsinformasjon
     }
 
     @Throws(PensjoninformasjonException::class, HttpServerErrorException::class, HttpClientErrorException::class)
