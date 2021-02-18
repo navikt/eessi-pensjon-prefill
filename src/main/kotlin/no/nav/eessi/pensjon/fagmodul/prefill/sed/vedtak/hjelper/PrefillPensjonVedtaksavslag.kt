685fd4bda2c0b516621326d943afd607ab780045
moamini <mo.amini@nav.no>
2021-02-18 11:03:36 +0100

    minor refactoring av PrefillPensjonVedtaksavslag

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/hjelper/PrefillPensjonVedtaksavslag.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/hjelper/PrefillPensjonVedtaksavslag.kt
@@ -31,11 +31,11 @@ object PrefillPensjonVedtaksavslag {
 
         val item = listOf(AvslagbegrunnelseItem(
 
-                //4.1.13.1
-                begrunnelse = avslagbegrunnelse,
+            //4.1.13.1
+            begrunnelse = avslagbegrunnelse,
 
-                //4.1.13.2 Other - Nei
-                annenbegrunnelse = null
+            //4.1.13.2 Other - Nei
+            annenbegrunnelse = null
         ))
 
         if (avslagbegrunnelse == null)
@@ -47,6 +47,18 @@ object PrefillPensjonVedtaksavslag {
     /**
      *  Kodeverk pr. jan 2021 https://confluence.adeo.no/pages/viewpage.action?pageId=338181329
      *  4.1.13.1 - Rejection reasons
+     *
+     *          4.1.[1].13.[1].1. Avslagsgrunner
+     *          [01] Ingen forsikringsperioder
+     *          [02] Forsikringsperiode på mindre enn ett år
+     *          [03] Krav til perioden eller andre kvalifiseringskrav er ikke oppfylt
+     *          [04] Ingen delvis uførhet eller funksjonshemming ble funnet
+     *          [05] Inntektsgrensen er overskredet
+     *          [06] Pensjonsalder er ikke nådd
+     *          [07] Manglende innformasjon fra søkeren
+     *          [08] Manglende deltakelse
+     *          [99] Andre grunner
+     *
      */
     fun createAvlsagsBegrunnelse(pendata: Pensjonsinformasjon): String? {
         logger.debug("4.1.13.1          AvlsagsBegrunnelse")
@@ -74,57 +86,33 @@ object PrefillPensjonVedtaksavslag {
         val erNedsattInntEvne = "NEDSATT_INNT_EVNE" == VedtakPensjonDataHelper.hentVilkarsvurderingUforetrygd(pendata).nedsattInntektsevne
         val erAlder = "ALDER" == VedtakPensjonDataHelper.hentVilkarsvurderingUforetrygd(pendata).alder
 
-//         Values in RINA v4.2
-//        4.1.[1].13.[1].1. Avslagsgrunner
-//        [01] Ingen forsikringsperioder
-//        [02] Forsikringsperiode på mindre enn ett år
-//        [03] Krav til perioden eller andre kvalifiseringskrav er ikke oppfylt
-//        [04] Ingen delvis uførhet eller funksjonshemming ble funnet
-//        [05] Inntektsgrensen er overskredet
-//        [06] Pensjonsalder er ikke nådd
-//        [07] Manglende innformasjon fra søkeren
-//        [08] Manglende deltakelse
-//        [99] Andre grunner
-
-        //ALDER eller ETTERLATTE eller BARNEP
-        if (KSAK.UFOREP != sakType && harBoddArbeidetUtland && erAvslagVilkarsproving) {
-            //pkt1 og pkt.9
-            if (erTrygdetidListeTom && !erMindreEnn3aar && !erMindreEnn1aar)
-                return "01"
-
-            //pkt.2 og pkt.10
-            if (erMindreEnn1aar || VedtakPensjonDataHelper.erTrygdeTid(pendata))
-                return "02"
-
-            if (erLavtTidligUttak || erMindreEnn3aar)
-                return "03"
-
-            if (erUnder62)
-                return "06"
+        when {
+            harBoddArbeidetUtland -> {
+                when {
+                    KSAK.UFOREP == sakType -> {
+                        when {
+                            erAlder && erAvslagVilkarsproving -> return "03"
+                            (erHensiktmessigBeh || erHensArbrettTiltak) && erAvslagVilkarsproving -> return "08"
+                            erNedsattInntEvne && erAvslagVilkarsproving -> return "04"
+                            VedtakPensjonDataHelper.erTrygdeTid(pendata) && erForutMedlem && erAvslagVilkarsproving -> return "02"
+                            pendata.trygdetidListe.trygdetidListe.isEmpty() && erForutMedlem && erAvslagVilkarsproving -> return "01"
+                        }
+                    }
+                    erAvslagVilkarsproving -> {
+                        //pkt1 og pkt.9
+                        when {
+                            erTrygdetidListeTom && !erMindreEnn3aar && !erMindreEnn1aar -> return "01"
+                            erMindreEnn1aar || VedtakPensjonDataHelper.erTrygdeTid(pendata) -> return "02"
+                            erLavtTidligUttak || erMindreEnn3aar -> return "03"
+                            erUnder62 -> return "06"
+                        }
+                    }
+                }
+                when {
+                    erIkkeMottattDok && erAvslagVilkarsproving -> return "07"
+                }
+            }
         }
-
-        //KUN UFOREP
-        if (KSAK.UFOREP == sakType && harBoddArbeidetUtland) {
-            //hentVilkarsvurderingUforetrygd
-            if (erAlder && erAvslagVilkarsproving)
-                return "03"
-
-            if ((erHensiktmessigBeh || erHensArbrettTiltak) && erAvslagVilkarsproving)
-                return "08"
-
-            if (erNedsattInntEvne && erAvslagVilkarsproving)
-                return "04"
-
-            if (VedtakPensjonDataHelper.erTrygdeTid(pendata) && erForutMedlem && erAvslagVilkarsproving)
-                return "02"
-            //pkt.5
-            if (pendata.trygdetidListe.trygdetidListe.isEmpty() && erForutMedlem && erAvslagVilkarsproving)
-                return "01"
-        }
-
-        //siste..   pendata.sak.sakType alle..
-        if (harBoddArbeidetUtland && erIkkeMottattDok && erAvslagVilkarsproving)
-            return "07"
 
         logger.debug("              -- Ingen avslagsbegrunnelse")
         return null
