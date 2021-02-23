373e840100ab8e630002a532d26a5ce1149502d7
moamini <mo.amini@nav.no>
2021-02-23 10:25:05 +0100

    prøver annen swagger config

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/config/SwaggerConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/config/SwaggerConfig.kt
@@ -2,17 +2,20 @@ package no.nav.eessi.pensjon.fagmodul.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Import
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
 import springfox.documentation.builders.RequestHandlerSelectors
 import springfox.documentation.service.ApiInfo
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc
 
 @Configuration
 @EnableSwagger2WebMvc
-class SwaggerConfig {
+@Import(value = [SpringDataRestConfiguration::class])
+    class SwaggerConfig {
 
     @Bean
     fun api(): Docket {
