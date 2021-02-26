288325511b8dcacc685c55da9a765ecafa0524b4
moamini <mo.amini@nav.no>
2021-02-26 09:08:32 +0100

    Fjerner "" i test funksjonsnavn

--- src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/helper/PrefillPensjonVedtaksavslagTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/fagmodul/prefill/sed/vedtak/helper/PrefillPensjonVedtaksavslagTest.kt
@@ -9,7 +9,7 @@ class PrefillPensjonVedtaksavslagTest {
 
 
     @Test
-    fun `forventer "01" på AvlsagsBegrunnelse for Alderpensjon TrygdleListeTom `() {
+    fun `forventer 01 på AvlsagsBegrunnelse for Alderpensjon TrygdleListeTom`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("ALDER").apply {
             vedtak.isBoddArbeidetUtland = true
             trygdetidListe.trygdetidListe.clear()
@@ -20,7 +20,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "01" på AvlsagsBegrunnelse for Gjenlevende TrygdleListeTom `() {
+    fun `forventer 01 på AvlsagsBegrunnelse for Gjenlevende TrygdleListeTom`() {
         val pendata1 = PensjonsinformasjonMother.pensjoninformasjonForSakstype("GJENLEV").apply {
             vedtak.isBoddArbeidetUtland = true
             trygdetidListe.trygdetidListe.clear()
@@ -31,7 +31,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "03" på AvlsagsBegrunnelse for AlderPensjon TrygdleListeTom, LAVT_TIDLIG_UTTAK`() {
+    fun `forventer 03 på AvlsagsBegrunnelse for AlderPensjon TrygdleListeTom, LAVT_TIDLIG_UTTAK`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("ALDER").apply {
             vedtak.isBoddArbeidetUtland = true
             vilkarsvurderingListe.vilkarsvurderingListe[0].avslagHovedytelse = "LAVT_TIDLIG_UTTAK"
@@ -42,7 +42,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "06" på AvlsagsBegrunnelse AlderPensjon TrygdleListeTom, UNDER_62`() {
+    fun `forventer 06 på AvlsagsBegrunnelse AlderPensjon TrygdleListeTom, UNDER_62`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("ALDER").apply {
             vilkarsvurderingListe.vilkarsvurderingListe.get(0).avslagHovedytelse = "UNDER_62"
             vilkarsvurderingListe.vilkarsvurderingListe.get(0).resultatHovedytelse = "AVSL"
@@ -51,7 +51,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "07" på AvlsagsBegrunnelse IKKE_MOTTATT_DOK`() {
+    fun `forventer 07 på AvlsagsBegrunnelse IKKE_MOTTATT_DOK`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("ALDER").apply {
             vilkarsvurderingListe.vilkarsvurderingListe[0].resultatHovedytelse = "AVSL"
             vilkarsvurderingListe.vilkarsvurderingListe[0].avslagHovedytelse = "IKKE_MOTTATT_DOK"
@@ -61,7 +61,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "01" på AvlsagsBegrunnelse Gjenlevendepensjon, TrygdleListeTom`() {
+    fun `forventer 01 på AvlsagsBegrunnelse Gjenlevendepensjon, TrygdleListeTom`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("GJENLEV").apply {
             vedtak.isBoddArbeidetUtland = true
             trygdetidListe.trygdetidListe.clear()
@@ -72,7 +72,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventer "03" på AvlsagsBegrunnelse Uførepensjon, TrygdleListeTom`() {
+    fun `forventer 03 på AvlsagsBegrunnelse Uførepensjon, TrygdleListeTom`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("UFOREP").apply {
             vedtak.isBoddArbeidetUtland = true
             trygdetidListe.trygdetidListe.clear()
@@ -83,7 +83,7 @@ class PrefillPensjonVedtaksavslagTest {
     }
 
     @Test
-    fun `forventet "08" ved  AvlsagsBegrunnelse på Uførepensjon ved TrygdleListeTom, HENSIKTSMESSIG_BEH`() {
+    fun `forventet 08 ved  AvlsagsBegrunnelse på Uførepensjon ved TrygdleListeTom, HENSIKTSMESSIG_BEH`() {
         val pendata = PensjonsinformasjonMother.pensjoninformasjonForSakstype("UFOREP").apply {
             vedtak.isBoddArbeidetUtland = true
             trygdetidListe.trygdetidListe.clear()
