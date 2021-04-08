3bb3acd61e43c908831ec430d3ef46e60630f981
mariampervez <mariam.pervez@nav.no>
2021-04-08 15:09:37 +0200

    Øker wait fra 10 til 14 for getBuc
    Co-authored-by: Jens Chr. Madsen <jens.christian.madsen@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/PrefillController.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/fagmodul/api/PrefillController.kt
@@ -78,7 +78,7 @@ class PrefillController(
 
         //wait 5 sec before getBuc metadata to UI
         try {
-            TimeUnit.SECONDS.sleep(10)
+            TimeUnit.SECONDS.sleep(14)
         } catch (ie: InterruptedException) {
             Thread.currentThread().interrupt()
         }
