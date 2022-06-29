af0b282bfb4b0f7a08b9fcb1ae4bf63d54aa8b6b
mariampervez <mariam.pervez@nav.no>
2022-06-29 11:13:09 +0200

    F håndterer advokat som kontakt i dødsboadresse
    Også refaktorert.
    
    Co-authored-by: Kjetil JD <kjetil.jorgensen-dahl@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/person/PreutfyllDodsboAdresse.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/person/PreutfyllDodsboAdresse.kt
@@ -2,19 +2,28 @@ package no.nav.eessi.pensjon.prefill.person
 
 import no.nav.eessi.pensjon.eux.model.sed.Adresse
 import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsbo
+import no.nav.eessi.pensjon.personoppslag.pdl.model.Personnavn
 
 internal fun preutfyllDodsboAdresse( kontaktinformasjonForDoedsbo: KontaktinformasjonForDoedsbo, landkode: String?): Adresse {
     val adresse = kontaktinformasjonForDoedsbo.adresse
-    val personnavn = kontaktinformasjonForDoedsbo.personSomKontakt!!.personnavn!!
+    val sammensattInfoForKontakt =
+        if (kontaktinformasjonForDoedsbo.personSomKontakt != null) {
+            (joinNavn(kontaktinformasjonForDoedsbo.personSomKontakt!!.personnavn!!)
+                + ", " + adresse.adresselinje1.replace("\n", " "))
+        } else {
+            val advokat = kontaktinformasjonForDoedsbo.advokatSomKontakt!!
+            (joinNavn(advokat.personnavn)
+                    + if (!advokat.organisasjonsnavn.isNullOrBlank()) ", " + advokat.organisasjonsnavn else { "" }
+                    + ", " + adresse.adresselinje1.replace("\n", " "))
+        }
     return Adresse(
-        gate = "Dødsbo v/"
-                + personnavn.fornavn
-                + if (personnavn.mellomnavn != null) { " " + personnavn.mellomnavn } else { "" }
-                + " " + personnavn.etternavn
-                + ", " + adresse.adresselinje1.replace("\n", " "),
+        gate = "Dødsbo v/$sammensattInfoForKontakt",
         bygning = adresse.adresselinje2?.replace("\n", " "),
         by = adresse.poststedsnavn,
         postnummer = adresse.postnummer,
         land = landkode
     )
 }
+
+private fun joinNavn(personnavn: Personnavn) =
+    (personnavn.fornavn + if (!personnavn.mellomnavn.isNullOrBlank()) " " + personnavn.mellomnavn else { "" } + " " + personnavn.etternavn)
--- src/test/kotlin/no/nav/eessi/pensjon/prefill/person/PreutfyllDodsboAdresseTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/prefill/person/PreutfyllDodsboAdresseTest.kt
@@ -4,6 +4,7 @@ import no.nav.eessi.pensjon.eux.model.sed.Adresse
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Folkeregistermetadata
 import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsbo
 import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsboAdresse
+import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsboAdvokatSomKontakt
 import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsboPersonSomKontakt
 import no.nav.eessi.pensjon.personoppslag.pdl.model.KontaktinformasjonForDoedsboSkifteform
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Metadata
@@ -15,16 +16,86 @@ import java.time.LocalDate
 internal class PreutfyllDodsboAdresseTest {
 
     @Test
-    fun `person med navn som kontakt`() {
+    fun `person som kontakt med navn`() {
+        val kontaktinformasjonForDoedsbo = createKontaktinformasjonForDoedsbo(
+            adresselinje1 = "Morgenstjerne Bakkemakker gate 351",
+            adresselinje2 = "Leilighet 4, oppgang 5",
+            postnummer = "6870",
+            poststedsnavn = "Gokk"
+        ).medPersonSomKontakt(fornavn = "Solsikke", mellomnavn = "Gaselle", etternavn = "Soltilbeder")
 
-        val kontaktinformasjonForDoedsbo = KontaktinformasjonForDoedsbo(
-            personSomKontakt = KontaktinformasjonForDoedsboPersonSomKontakt(
-                personnavn = Personnavn("Solsikke", "Gaselle",  "Soltilbeder")),
-            adresse =  KontaktinformasjonForDoedsboAdresse(
-                adresselinje1 = "Morgenstjerne Bakkemakker gate 351",
-                adresselinje2 = "Leilighet 4, oppgang 5",
-                postnummer = "68",
-                poststedsnavn = "Gokk"
+        val actual = preutfyllDodsboAdresse(kontaktinformasjonForDoedsbo, null)
+
+        val expected = Adresse(
+            gate = "Dødsbo v/Solsikke Gaselle Soltilbeder, Morgenstjerne Bakkemakker gate 351",
+            bygning = "Leilighet 4, oppgang 5",
+            by = "Gokk",
+            postnummer = "6870",
+            land = null
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `advokat som kontakt med organisasjonsnavn`() {
+        val kontaktinformasjonForDoedsbo = createKontaktinformasjonForDoedsbo(
+            adresselinje1 = "Tobben Tiedemanns gate 2",
+            adresselinje2 = "Suite 456",
+            postnummer = "7070",
+            poststedsnavn = "Verdal"
+        ).medAdvokatSomKontakt(
+            fornavn = "Lurendreier",
+            etternavn = "Fantestreker",
+            organisasjon = "Bakke, Motbakke, Vannfall, Tørketid Advokater ANS"
+        )
+        val actual = preutfyllDodsboAdresse(kontaktinformasjonForDoedsbo, null)
+
+        val expected = Adresse(
+            gate = "Dødsbo v/Lurendreier Fantestreker, Bakke, Motbakke, Vannfall, Tørketid Advokater ANS, Tobben Tiedemanns gate 2",
+            bygning = "Suite 456",
+            by = "Verdal",
+            postnummer = "7070",
+            land = null
+        )
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun `advokat som kontakt uten organisasjonsnavn`() {
+        val kontaktinformasjonForDoedsbo = createKontaktinformasjonForDoedsbo(
+            adresselinje1 = "c/o Mentor Nestor Arnesen",
+            adresselinje2 = "Barbakken 4",
+            postnummer = "7080",
+            poststedsnavn = "Leikanger"
+        ).medAdvokatSomKontakt(
+            fornavn = "Nestor",
+            mellomnavn = "Mentor",
+            etternavn = "Mortensen"
+        )
+
+        val actual = preutfyllDodsboAdresse(kontaktinformasjonForDoedsbo, null)
+
+        val expected = Adresse(
+            gate = "Dødsbo v/Nestor Mentor Mortensen, c/o Mentor Nestor Arnesen",
+            bygning = "Barbakken 4",
+            by = "Leikanger",
+            postnummer = "7080",
+            land = null
+        )
+        assertEquals(expected, actual)
+    }
+
+    fun createKontaktinformasjonForDoedsbo(
+        adresselinje1: String,
+        adresselinje2: String? = null,
+        postnummer: String,
+        poststedsnavn: String
+    ) = KontaktinformasjonForDoedsbo(
+            adresse = KontaktinformasjonForDoedsboAdresse(
+                adresselinje1 = adresselinje1,
+                adresselinje2 = adresselinje2,
+                postnummer = postnummer,
+                poststedsnavn = poststedsnavn
             ),
             attestutstedelsesdato = LocalDate.now(),
             folkeregistermetadata = Folkeregistermetadata(null),
@@ -36,16 +107,35 @@ internal class PreutfyllDodsboAdresseTest {
             ),
             skifteform = KontaktinformasjonForDoedsboSkifteform.OFFENTLIG
         )
-        val actual = preutfyllDodsboAdresse(kontaktinformasjonForDoedsbo, null)
 
-        val expected = Adresse(
-            gate = "Dødsbo v/Solsikke Gaselle Soltilbeder, Morgenstjerne Bakkemakker gate 351",
-            bygning = "Leilighet 4, oppgang 5",
-            by = "Gokk",
-            postnummer = "68",
-            land = null
+    fun KontaktinformasjonForDoedsbo.medPersonSomKontakt(
+        fornavn: String,
+        mellomnavn: String? = null,
+        etternavn: String
+    ) = this.copy(
+            personSomKontakt = KontaktinformasjonForDoedsboPersonSomKontakt(
+                personnavn = Personnavn(
+                    fornavn = fornavn,
+                    mellomnavn = mellomnavn,
+                    etternavn = etternavn
+                )
+            )
         )
-        assertEquals(expected, actual)
 
-    }
+    fun KontaktinformasjonForDoedsbo.medAdvokatSomKontakt(
+        fornavn: String,
+        mellomnavn: String? = null,
+        etternavn: String,
+        organisasjon: String? = null
+    ) = this.copy(
+            advokatSomKontakt = KontaktinformasjonForDoedsboAdvokatSomKontakt(
+                personnavn = Personnavn(
+                    fornavn = fornavn,
+                    mellomnavn = mellomnavn,
+                    etternavn = etternavn
+                ),
+                organisasjonsnavn = organisasjon
+            )
+        )
+
 }
