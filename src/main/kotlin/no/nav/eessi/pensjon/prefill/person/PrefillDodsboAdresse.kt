3f851610b59eeba03808017811e549757a7b6815
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-07-01 14:47:58 +0200

    R ryddet i PrefillDoedsboAdresse
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Daniel Skarpås <daniel.skarpas@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/person/PrefillDodsboAdresse.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/person/PrefillDodsboAdresse.kt
@@ -8,63 +8,80 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 class PrefillDodsboAdresse {
-    internal fun preutfyllDodsboAdresse( kontaktinformasjonForDoedsbo: KontaktinformasjonForDoedsbo, landkode: String?, personnavnProvider: (identifikasjonsnummer: String) -> Navn): Adresse {
 
-        val logger: Logger by lazy { LoggerFactory.getLogger("no.nav.eessi.pensjon.prefill.person.preutfyllDodsboAdresse") }
+    val logger: Logger by lazy { LoggerFactory.getLogger(PrefillDodsboAdresse::class.java) }
 
-        val adresse = kontaktinformasjonForDoedsbo.adresse
-        val sammensattInfoForKontakt =
+    internal fun preutfyllDodsboAdresse(
+        kontaktinfo: KontaktinformasjonForDoedsbo,
+        landkode: String?,
+        personnavnProvider: (identifikasjonsnummer: String) -> Navn
+    ): Adresse {
+
+        val gateFeltVerdiFullLengde = "Dødsbo v/" +
             when {
-                kontaktinformasjonForDoedsbo.personSomKontakt != null -> {
-                    val joinetNavn: String =
-                        if (kontaktinformasjonForDoedsbo.personSomKontakt!!.personnavn != null) {
-                            joinNavn(kontaktinformasjonForDoedsbo.personSomKontakt!!.personnavn!!)
-                        } else {
-                            val navn = personnavnProvider(kontaktinformasjonForDoedsbo.personSomKontakt!!.identifikasjonsnummer!!)
-                            ((navn.fornavn + if (!navn.mellomnavn.isNullOrBlank()) " " + navn.mellomnavn else { "" } + " " + navn.etternavn))
-                        }
-                    (joinetNavn + ", " + adresse.adresselinje1.replace("\n", " "))
-                }
-                kontaktinformasjonForDoedsbo.advokatSomKontakt != null -> {
-                    val advokat = kontaktinformasjonForDoedsbo.advokatSomKontakt!!
-                    (joinNavn(advokat.personnavn)
-                            + if (!advokat.organisasjonsnavn.isNullOrBlank()) ", " + advokat.organisasjonsnavn else { "" }
-                            + ", " + adresse.adresselinje1.replace("\n", " "))
-                }
-                else -> {
-                    val organisasjon = kontaktinformasjonForDoedsbo.organisasjonSomKontakt!!
-                    (if (organisasjon.kontaktperson != null) joinNavn(organisasjon.kontaktperson!!) + ", " else { "" }
-                            + organisasjon.organisasjonsnavn
-                            + ", " + adresse.adresselinje1.replace("\n", " "))
-                }
+                kontaktinfo.personSomKontakt != null -> settSammenPersonInfo(kontaktinfo, personnavnProvider)
+                kontaktinfo.advokatSomKontakt != null -> settSammenAdvokatInfo(kontaktinfo)
+                else -> settSammenOrganisasjonInfo(kontaktinfo)
             }
-        val gateFeltVerdi = "Dødsbo v/$sammensattInfoForKontakt"
-        val gateFeltVerdiTrunkert =
-            if (gateFeltVerdi.length <= 155) {
-                gateFeltVerdi
-            } else {
-                val trunkert = gateFeltVerdi.substring(0, 155)
-                logger.error("KontaktinformasjonForDoedsbo-felter for lange for SED sitt gate-felt, verdien er avkortet til 155 tegn.\n" +
-                        "Person: ${kontaktinformasjonForDoedsbo.personSomKontakt}\n" +
-                        "Advokat: ${kontaktinformasjonForDoedsbo.advokatSomKontakt}\n" +
-                        "Organisasjon: ${kontaktinformasjonForDoedsbo.organisasjonSomKontakt}\n" +
-                        "Adresse: ${kontaktinformasjonForDoedsbo.adresse}\n" +
-                        "Resultat: ${trunkert}\n" +
+
+        if (gateFeltVerdiFullLengde.length > 155) {
+            logger.error(
+                "KontaktinformasjonForDoedsbo-felter for lange for SED sitt gate-felt, verdien er avkortet til 155 tegn.\n" +
+                        "Person: ${kontaktinfo.personSomKontakt}\n" +
+                        "Advokat: ${kontaktinfo.advokatSomKontakt}\n" +
+                        "Organisasjon: ${kontaktinfo.organisasjonSomKontakt}\n" +
+                        "Adresse: ${kontaktinfo.adresse}\n" +
+                        "Resultat: ${maks155tegn(gateFeltVerdiFullLengde)}\n" +
                         "Feilen logges - vi fortsetter med 155 tegn."
-                )
-                trunkert
-            }
+            )
+        }
+
+        val gateFeltVerdi = maks155tegn(gateFeltVerdiFullLengde)
 
         return Adresse(
-            gate = gateFeltVerdiTrunkert,
-            bygning = adresse.adresselinje2?.replace("\n", " "),
-            by = adresse.poststedsnavn,
-            postnummer = adresse.postnummer,
+            gate = gateFeltVerdi,
+            bygning = kontaktinfo.adresse.adresselinje2?.replace("\n", " "),
+            by = kontaktinfo.adresse.poststedsnavn,
+            postnummer = kontaktinfo.adresse.postnummer,
             land = landkode
         )
+    }
+
+    private fun maks155tegn(gateFeltVerdi: String) =
+        if (gateFeltVerdi.length <= 155) gateFeltVerdi else gateFeltVerdi.substring(0, 155)
+
+    private fun settSammenPersonInfo(
+        kontaktinfo: KontaktinformasjonForDoedsbo,
+        personnavnProvider: (identifikasjonsnummer: String) -> Navn
+    ): String {
+        val person = kontaktinfo.personSomKontakt!!
+        val joinetNavn: String =
+            if (person.personnavn != null) {
+                joinNavn(person.personnavn!!)
+            } else {
+                val navn = personnavnProvider(person.identifikasjonsnummer!!)
+                (navn.fornavn + if (!navn.mellomnavn.isNullOrBlank()) " " + navn.mellomnavn else { "" } + " " + navn.etternavn)
+            }
+        return (joinetNavn + ", " + kontaktinfo.adresse.adresselinje1.utenLinjeskift())
+    }
+
+    private fun settSammenAdvokatInfo(kontaktinfo: KontaktinformasjonForDoedsbo): String {
+        val advokat = kontaktinfo.advokatSomKontakt!!
+        return (joinNavn(advokat.personnavn)
+                + if (!advokat.organisasjonsnavn.isNullOrBlank()) ", " + advokat.organisasjonsnavn else { "" }
+                + ", " + kontaktinfo.adresse.adresselinje1.utenLinjeskift())
+    }
+
+    private fun settSammenOrganisasjonInfo(kontaktinfo: KontaktinformasjonForDoedsbo): String {
+        val organisasjon = kontaktinfo.organisasjonSomKontakt!!
+        return (if (organisasjon.kontaktperson != null) joinNavn(organisasjon.kontaktperson!!) + ", " else { "" }
+                        + organisasjon.organisasjonsnavn
+                        + ", " + kontaktinfo.adresse.adresselinje1.utenLinjeskift())
     }
 
     private fun joinNavn(personnavn: Personnavn) =
         (personnavn.fornavn
                 + if (!personnavn.mellomnavn.isNullOrBlank()) " " + personnavn.mellomnavn else { "" } + " " + personnavn.etternavn)
 }
+
+private fun String.utenLinjeskift() = this.replace("\n", " ")
