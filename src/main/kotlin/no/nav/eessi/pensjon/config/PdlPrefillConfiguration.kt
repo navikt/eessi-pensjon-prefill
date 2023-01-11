6340d9695b04df75f1858712a4909f8354eb699d
Daniel Skarpås <dskarpas@gmail.com>
2023-01-11 14:20:07 +0100

    R - Endrer fra hierarkisk init av PdlConfig til kun en instans

--- src/main/kotlin/no/nav/eessi/pensjon/config/PdlPrefillConfiguration.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/PdlPrefillConfiguration.kt
@@ -2,22 +2,20 @@ package no.nav.eessi.pensjon.config
 
 import no.nav.common.token_client.builder.AzureAdTokenClientBuilder
 import no.nav.common.token_client.client.AzureAdOnBehalfOfTokenClient
+import no.nav.eessi.pensjon.personoppslag.pdl.PdlConfiguration
 import no.nav.eessi.pensjon.personoppslag.pdl.PdlToken
 import no.nav.eessi.pensjon.personoppslag.pdl.PdlTokenCallBack
 import no.nav.eessi.pensjon.personoppslag.pdl.PdlTokenImp
 import no.nav.eessi.pensjon.utils.getToken
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.context.annotation.Primary
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
-import org.springframework.core.Ordered
-import org.springframework.core.annotation.Order
-import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
 
-@Component("PdlTokenComponent")
 @Profile("prod", "test")
-@Primary
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Configuration
 class PdlPrefillTokenComponent(
     private val tokenValidationContextHolder: TokenValidationContextHolder,
     @Value("\${AZURE_APP_PDL_CLIENT_ID}") private val pdlClientId: String): PdlTokenCallBack {
@@ -36,5 +34,9 @@ class PdlPrefillTokenComponent(
         )
 
         return PdlTokenImp(accessToken)
+    }
+    @Bean
+    fun pdlRestTemplate(): RestTemplate {
+        return PdlConfiguration().pdlRestTemplate(this)
     }
 }
