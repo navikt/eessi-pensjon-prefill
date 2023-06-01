751cf5f600b068153bb02a3797674a89bd560982
Daniel Skarpås <dskarpas@gmail.com>
2023-06-01 08:56:06 +0200

    t - Fjerner sjekk for alle klasser da det allerede er sjekker for produksjon og test

--- src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/architecture/ArchitectureTest.kt
@@ -6,14 +6,16 @@ import com.tngtech.archunit.core.domain.JavaClass
 import com.tngtech.archunit.core.domain.JavaClasses
 import com.tngtech.archunit.core.importer.ClassFileImporter
 import com.tngtech.archunit.core.importer.ImportOption
-import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noFields
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noMethods
 import com.tngtech.archunit.library.Architectures.layeredArchitecture
 import com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices
 import no.nav.eessi.pensjon.EessiPrefillApplication
 import no.nav.eessi.pensjon.metrics.MetricsHelper
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.context.annotation.Scope
 import org.springframework.web.bind.annotation.RestController
@@ -37,8 +39,6 @@ class ArchitectureTest {
         @BeforeAll @JvmStatic
         fun `extract classes`() {
             allClasses = ClassFileImporter().importPackages(root)
-
-            assertTrue(allClasses.size in 250..1100, "Sanity check on no. of classes to analyze (is ${allClasses.size})")
 
             productionClasses = ClassFileImporter()
                 .withImportOption(ImportOption.DoNotIncludeTests())
