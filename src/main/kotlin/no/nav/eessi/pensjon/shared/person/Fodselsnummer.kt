77c0f3119a3588a2f68fc25bf1d9d7b4b1e91f01
mariampervez <mariam.pervez@nav.no>
2023-09-22 14:17:16 +0200

    r - Reverter Sletter bestemIdent fra shared folder
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/shared/person/Fodselsnummer.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/shared/person/Fodselsnummer.kt
@@ -161,7 +161,6 @@ class Fodselsnummer private constructor(@JsonValue val value: String) {
             val res = 11 - (sum % 11)
             return if (res == 11) 0 else res
         }
-    }
 
     enum class Kjoenn {
         MANN,
