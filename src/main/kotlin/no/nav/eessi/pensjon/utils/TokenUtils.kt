2c6e8d14f14b6ab54ae69cc49006afa9cac292e5
Daniel Skarpås <dskarpas@gmail.com>
2024-01-15 08:49:30 +0100

    R - Sletting av ubrukt kode

--- src/main/kotlin/no/nav/eessi/pensjon/utils/TokenUtils.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/utils/TokenUtils.kt
@@ -2,32 +2,9 @@ package no.nav.eessi.pensjon.utils
 
 import no.nav.security.token.support.core.context.TokenValidationContextHolder
 import no.nav.security.token.support.core.jwt.JwtToken
-import no.nav.security.token.support.core.jwt.JwtTokenClaims
-import java.util.*
 
 
- fun getClaims(tokenValidationContextHolder: TokenValidationContextHolder): JwtTokenClaims {
-    val context = tokenValidationContextHolder.getTokenValidationContext()
-    if(context.issuers.isEmpty())
-        throw RuntimeException("No issuer found in context")
-
-    val validIssuer = context.issuers.filterNot { issuer ->
-        val oidcClaims = context.getClaims(issuer)
-        oidcClaims.expirationTime.before(Date())
-    }.map { it }
-
-
-    if (validIssuer.isNotEmpty()) {
-        val issuer = validIssuer.first()
-        return context.getClaims(issuer)
-    }
-    throw RuntimeException("No valid issuer found in context")
-
-}
-
- fun getNavIdent(tokenValidationContextHolder: TokenValidationContextHolder) = getClaims(tokenValidationContextHolder).get("NAVident")?.toString()
-
- fun getToken(tokenValidationContextHolder: TokenValidationContextHolder): JwtToken {
+fun getToken(tokenValidationContextHolder: TokenValidationContextHolder): JwtToken {
     val context = tokenValidationContextHolder.getTokenValidationContext()
     if(context.issuers.isEmpty())
         throw RuntimeException("No issuer found in context")
