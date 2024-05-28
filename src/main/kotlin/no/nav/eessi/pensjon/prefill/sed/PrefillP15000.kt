35496dd3fa4a6e73bdc22c74fba46ba380a72d68
mariampervez <mariam.pervez@nav.no>
2024-05-28 09:43:43 +0200

    r - Kravtype er enum nå i stedet for String i Krav

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/PrefillP15000.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/PrefillP15000.kt
@@ -1,17 +1,7 @@
 package no.nav.eessi.pensjon.prefill.sed
 
-import no.nav.eessi.pensjon.eux.model.sed.Bruker
-import no.nav.eessi.pensjon.eux.model.sed.Krav
-import no.nav.eessi.pensjon.eux.model.sed.KravType
-import no.nav.eessi.pensjon.eux.model.sed.KravType.ALDER
-import no.nav.eessi.pensjon.eux.model.sed.KravType.GJENLEV
-import no.nav.eessi.pensjon.eux.model.sed.KravType.UFOREP
-import no.nav.eessi.pensjon.eux.model.sed.Nav
-import no.nav.eessi.pensjon.eux.model.sed.P15000
-import no.nav.eessi.pensjon.eux.model.sed.P15000Pensjon
-import no.nav.eessi.pensjon.eux.model.sed.Person
-import no.nav.eessi.pensjon.eux.model.sed.PinItem
-import no.nav.eessi.pensjon.eux.model.sed.RelasjonAvdodItem
+import no.nav.eessi.pensjon.eux.model.sed.*
+import no.nav.eessi.pensjon.eux.model.sed.KravType.*
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Familierelasjonsrolle
 import no.nav.eessi.pensjon.personoppslag.pdl.model.Sivilstandstype
 import no.nav.eessi.pensjon.prefill.models.PersonDataCollection
@@ -80,7 +70,7 @@ class PrefillP15000(private val prefillSed: PrefillSed) {
         val forsikretPersonPin = forsikretPerson?.pin?.firstOrNull()
         val adresse = forsikretBruker?.adresse
 
-        val krav = Krav(dato = prefillData.kravDato?.let { validateFrontEndKravDato(it).toString() }, type = kravType.verdi)
+        val krav = Krav(dato = prefillData.kravDato?.let { validateFrontEndKravDato(it).toString() }, type = kravType)
 
         val nav = Nav(
             eessisak = eessielm,
