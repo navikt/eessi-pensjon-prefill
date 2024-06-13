648af6c7b0e89ed9623ca46b1b0ba386baf0fb99
Daniel Skarpås <dskarpas@gmail.com>
2024-06-13 10:42:00 +0200

    B - Sender null til når det er feil med brukerssaksliste

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/PensjonsinformasjonService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/PensjonsinformasjonService.kt
@@ -35,8 +35,8 @@ class PensjonsinformasjonService(private val pensjonsinformasjonClient: Pensjons
     companion object {
         //hjelpe metode for å hente ut valgt V1SAK på vetak/SAK fnr og sakid benyttes
         fun finnSak(sakId: String?, pendata: Pensjonsinformasjon): V1Sak? {
-            if (sakId.isNullOrBlank()) throw ManglendeSakIdException("Mangler sakId")
-            return FinnSak.finnSak(sakId, pendata)
+            return if (sakId.isNullOrBlank() || pendata.brukersSakerListe.brukersSakerListe == null) null
+            else FinnSak.finnSak(sakId, pendata)
         }
     }
 
