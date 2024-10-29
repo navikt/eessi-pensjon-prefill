ce7acf0680b3e156dcdb022104b8f16a29be9688
Daniel Skarpås <dskarpas@gmail.com>
2024-10-29 12:41:56 +0100

    T - Retter test etter innføring av PersonInfo og prefilling av kontakt for person

--- src/test/kotlin/no/nav/eessi/pensjon/integrationtest/sed/PrefillP15000IntegrationTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/integrationtest/sed/PrefillP15000IntegrationTest.kt
@@ -637,7 +637,16 @@ class PrefillP15000IntegrationTest {
                     "fornavn" : "Lever",
                     "kjoenn" : "M",
                     "foedselsdato" : "1988-07-12",
-                    "rolle" : "01"
+                    "rolle" : "01",
+                    "kontakt" : {
+                      "telefon" : [ {
+                        "type" : "mobil",
+                        "nummer" : "11111111"
+                      } ],
+                      "email" : [ {
+                        "adresse" : "melleby11@melby.no"
+                      } ]
+                    }
                   },
                   "adresse" : {
                     "gate" : "Oppoverbakken 66",
