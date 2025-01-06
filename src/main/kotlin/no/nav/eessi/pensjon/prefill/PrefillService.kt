06ada72de543ca74ac2cc5ec7902d67599ae7b3c
Daniel Skarpås <dskarpas@gmail.com>
2025-01-06 09:29:20 +0100

    F - Legger inn håndtering av andre exceptions enn  HttpClientErrorException not found

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/PrefillService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/PrefillService.kt
@@ -85,13 +85,17 @@ class PrefillService(
     }
 
     private fun hentKrrPerson(norskIdent: String, request: ApiRequest): PersonInfo {
-        val krrPerson = krrService.hentPersonFraKrr(norskIdent).let { personResponse ->
+        val krrPerson = krrService.hentPersonFraKrr(norskIdent)?.let { personResponse ->
             KrrPerson(
-                reservert = personResponse?.reservert,
-                epostadresse = personResponse?.epostadresse.validateEmail(request.processDefinitionVersion),
-                mobiltelefonnummer = personResponse?.mobiltelefonnummer
-            )
-        }
+                reservert = personResponse.reservert,
+                epostadresse = personResponse.epostadresse.validateEmail(request.processDefinitionVersion),
+                mobiltelefonnummer = personResponse.mobiltelefonnummer
+            ).also { logger.debug("KrrPerson: ${it.toJson()}") }
+        } ?: KrrPerson(
+            reservert = false,
+            epostadresse = null,
+            mobiltelefonnummer = null
+        )
 
         val personInfo = if (krrPerson.reservert == true) {
             PersonInfo(
