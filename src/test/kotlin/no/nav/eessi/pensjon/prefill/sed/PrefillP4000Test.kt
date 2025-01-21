17ad5c27080317c17a403f98ed7c75270e0220d8
mariampervez <mariam.pervez@nav.no>
2025-01-21 14:41:45 +0100

    t - Legger til testing for preutfylling av uid som ikke er et EU land

--- src/test/kotlin/no/nav/eessi/pensjon/prefill/sed/PrefillP4000Test.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/prefill/sed/PrefillP4000Test.kt
@@ -2,7 +2,9 @@ package no.nav.eessi.pensjon.prefill.sed
 
 import io.mockk.every
 import io.mockk.mockk
+import no.nav.eessi.pensjon.personoppslag.pdl.model.UtenlandskIdentifikasjonsnummer
 import no.nav.eessi.pensjon.prefill.PersonPDLMock
+import no.nav.eessi.pensjon.prefill.PersonPDLMock.mockMeta
 import no.nav.eessi.pensjon.prefill.models.PersonDataCollection
 import no.nav.eessi.pensjon.prefill.person.PrefillPDLNav
 import no.nav.eessi.pensjon.prefill.person.PrefillSed
@@ -10,6 +12,7 @@ import no.nav.eessi.pensjon.shared.api.ApiRequest
 import no.nav.eessi.pensjon.shared.api.PersonInfo
 import no.nav.eessi.pensjon.shared.person.FodselsnummerGenerator
 import no.nav.eessi.pensjon.utils.mapJsonToAny
+import no.nav.eessi.pensjon.utils.toJson
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -25,6 +28,7 @@ class PrefillP4000Test {
         prefillNav = PrefillPDLNav(
             prefillAdresse = mockk {
                 every { hentLandkode(any()) } returns "NO"
+                every { hentLandkode(eq("AUT")) } returns "AU"
                 every { createPersonAdresse(any()) } returns mockk(relaxed = true)
             },
             institutionid = "NO:noinst002",
@@ -37,16 +41,56 @@ class PrefillP4000Test {
     fun `Ser at P4000 prefiller med gjenlevende`() {
         val apiRequest: ApiRequest =  mapJsonToAny<ApiRequest>(apiRequest())
         val data = ApiRequest.buildPrefillDataModelOnExisting(
-            apiRequest.copy(payload = javaClass.getResource("/json/nav/P4000-NAV.json").readText()),
+            apiRequest.copy(payload = javaClass.getResource("/json/nav/P4000-NAV.json")!!.readText()),
             PersonInfo("12345", apiRequest.aktoerId!!), personFnr
         )
+
         val personData = PersonDataCollection(
-            forsikretPerson = PersonPDLMock.createWith(),
+            forsikretPerson = PersonPDLMock.createWith().copy(utenlandskIdentifikasjonsnummer = listOf(
+                UtenlandskIdentifikasjonsnummer(
+                    "123123123",
+                    "AUT",
+                    false,
+                    metadata = mockMeta(
+                    )
+                )
+        )),
             gjenlevendeEllerAvdod = PersonPDLMock.createWith()
         )
 
         val sed = p4000.prefill(data, personData)
+
         assertEquals("3123", sed.p4000Pensjon?.gjenlevende?.person?.pin?.get(0)?.identifikator)
+        assertEquals("123123123", sed.p4000Pensjon?.gjenlevende?.person?.pin?.firstOrNull { it.land == "AU" }?.identifikator)
+        assertEquals(null, sed.nav?.bruker?.person?.pin?.firstOrNull { it.land == "AU" }?.identifikator)
+    }
+
+    @Test
+    fun `Ser at P4000 prefiller forsikret uten uid fra USA som ikke er et EU land`() {
+        val apiRequest: ApiRequest =  mapJsonToAny<ApiRequest>(apiRequest())
+        val data = ApiRequest.buildPrefillDataModelOnExisting(
+            apiRequest.copy(payload = javaClass.getResource("/json/nav/P4000-NAV.json")!!.readText()),
+            PersonInfo("12345", apiRequest.aktoerId!!), personFnr
+        )
+
+        val personData = PersonDataCollection(
+            forsikretPerson = PersonPDLMock.createWith().copy(utenlandskIdentifikasjonsnummer = listOf(
+                UtenlandskIdentifikasjonsnummer(
+                    "123123123",
+                    "USA",
+                    false,
+                    metadata = mockMeta(
+                    )
+                )
+            )),
+            gjenlevendeEllerAvdod = PersonPDLMock.createWith()
+        )
+
+        val sed = p4000.prefill(data, personData)
+
+        assertEquals("3123", sed.p4000Pensjon?.gjenlevende?.person?.pin?.get(0)?.identifikator)
+        assertEquals(null, sed.p4000Pensjon?.gjenlevende?.person?.pin?.firstOrNull { it.land == "US" }?.identifikator)
+        assertEquals(null, sed.nav?.bruker?.person?.pin?.firstOrNull { it.land == "US" }?.identifikator)
     }
 
     fun apiRequest(): String {
