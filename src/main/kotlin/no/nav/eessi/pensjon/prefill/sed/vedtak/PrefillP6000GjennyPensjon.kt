bb45554e22cd80ae1a70104928b3a1eddc5648a9
Daniel Skarpås <dskarpas@gmail.com>
2025-07-22 10:34:34 +0200

    R - Sletter kommentert kode

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/vedtak/PrefillP6000GjennyPensjon.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/vedtak/PrefillP6000GjennyPensjon.kt
@@ -5,33 +5,17 @@ import no.nav.eessi.pensjon.eux.model.sed.P6000Pensjon
 import no.nav.eessi.pensjon.eux.model.sed.VedtakItem
 import no.nav.eessi.pensjon.prefill.EtterlatteService
 
-class PrefillP6000GjennyPensjon(
-) {
+class PrefillP6000GjennyPensjon {
 
     fun prefillP6000GjennyPensjon(
         gjenlevende: Bruker?,
         listeMedVedtak: List<VedtakItem>?,
     ): P6000Pensjon? {
-//        val resultatEtterlatteRespData =
-//            etterlatteService.hentGjennyVedtak(gjenlevende?.person?.pin?.first()?.identifikator!!)
         if (listeMedVedtak.isNullOrEmpty()) return null
-        else
-        {
-//            val listeMedVedtak = resultatEtterlatteRespData.getOrNull()?.vedtak?.map { vedtak ->
-//                VedtakItem(
-//                    virkningsdato = vedtak.virkningstidspunkt.toString(),
-//                    beregning = vedtak.utbetaling.map {
-//                        BeregningItem(
-//                            beloepBrutto = BeloepBrutto(beloep = it.beloep)
-//                        )
-//                    }
-//                )
-//            }
-            return P6000Pensjon(
-                gjenlevende = gjenlevende,
-                vedtak = listeMedVedtak
-            )
-        }
+        return P6000Pensjon(
+            gjenlevende = gjenlevende,
+            vedtak = listeMedVedtak
+        )
     }
 
 }
