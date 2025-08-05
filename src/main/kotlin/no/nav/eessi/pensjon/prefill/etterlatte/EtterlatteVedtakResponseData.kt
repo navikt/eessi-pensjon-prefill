f2cb5d65e359e4764ae033eb919f84f050b93e9c
Daniel Skarpås <dskarpas@gmail.com>
2025-08-05 14:17:36 +0200

    F - Flytter EtterlatteService til egen klasse sammen med modellen (rettelse etter feil i archtest)

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/etterlatte/EtterlatteVedtakResponseData.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/etterlatte/EtterlatteVedtakResponseData.kt
@@ -9,26 +9,22 @@ import no.nav.eessi.pensjon.eux.model.sed.VedtakItem
 data class EtterlatteVedtakResponseData(
     val vedtak: List<GjennyVedtak>
 ) {
-    fun hentVedtakItems(): List<VedtakItem>? {
-        return vedtak.map { vedtak ->
-            VedtakItem(
-                virkningsdato = vedtak.virkningstidspunkt.toString(),
-                resultat = vedtak.type?.value,
-                type = "03", // Etterlatte pensjon
-                beregning = vedtak.utbetaling?.map {
-                    BeregningItem(
-                        beloepBrutto = BeloepBrutto(beloep = it.beloep),
-                        periode = Periode(
-                            fom = it.fraOgMed.toString(),
-                            tom = it.tilOgMed?.toString() ?: ""
-                        )
+    fun hentVedtakItems(): List<VedtakItem>? = vedtak.map { vedtak ->
+        VedtakItem(
+            virkningsdato = vedtak.virkningstidspunkt.toString(),
+            resultat = vedtak.type?.value,
+            type = "03", // Etterlatte pensjon
+            beregning = vedtak.utbetaling?.map {
+                BeregningItem(
+                    beloepBrutto = BeloepBrutto(beloep = it.beloep),
+                    periode = Periode(
+                        fom = it.fraOgMed.toString(),
+                        tom = it.tilOgMed?.toString() ?: ""
                     )
-                }
-            )
-        }
+                )
+            }
+        )
     }
 
-    fun hentFørsteSakType(): String? {
-        return vedtak.firstOrNull { it.sakType != null }?.sakType
-    }
+    fun hentFørsteSakType(): String? = vedtak.firstOrNull { it.sakType != null }?.sakType
 }
