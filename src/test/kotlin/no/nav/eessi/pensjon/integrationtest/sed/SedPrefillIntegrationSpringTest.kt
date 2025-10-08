a612f0f1acdc4ff00a389d894c61141edb519aa6
Daniel Skarpås <dskarpas@gmail.com>
2025-10-08 10:21:46 +0200

    T - Retter test etter utvidelse for sluttbehandling

--- src/test/kotlin/no/nav/eessi/pensjon/integrationtest/sed/SedPrefillIntegrationSpringTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/integrationtest/sed/SedPrefillIntegrationSpringTest.kt
@@ -114,7 +114,7 @@ class SedPrefillIntegrationSpringTest {
             "med alder with uføre pensjondata throw error bad request, /pensjonsinformasjon/krav/P2200-UP-INNV.xml, " +
                     "Du kan ikke opprette alderspensjonskrav i en uføretrygdsak (PESYS-saksnr: 22874955 har sakstype UFOREP",
             "med sak fra GJENLEV feiler, /pensjonsinformasjon/krav/GJ_P2000_BH_MED_UTL.xml, " +
-                    "Det finnes ingen iverksatte vedtak for førstegangsbehandling kun utland. Vennligst gå til EESSI-Pensjon fra vedtakskontekst."],
+                    "Det finnes ingen iverksatte vedtak for førstegangsbehandling kun utland, eller sluttbehandling. Vennligst gå til EESSI-Pensjon fra vedtakskontekst."],
         nullValues = ["null"]
     )
     @Throws(Exception::class)
