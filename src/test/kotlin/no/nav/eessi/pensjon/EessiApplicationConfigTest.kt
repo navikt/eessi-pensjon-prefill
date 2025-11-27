d60de9a926b804390aa3417f7b00f649332127a8
Daniel Skarpås <dskarpas@gmail.com>
2025-11-27 08:14:18 +0100

    T - Fix for contextTest

--- src/test/kotlin/no/nav/eessi/pensjon/EessiApplicationConfigTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/EessiApplicationConfigTest.kt
@@ -4,24 +4,24 @@ import com.ninjasquad.springmockk.MockkBean
 import com.ninjasquad.springmockk.MockkBeans
 import no.nav.eessi.pensjon.config.RestTemplateConfig
 import no.nav.eessi.pensjon.kodeverk.KodeverkClient
-import no.nav.security.token.support.spring.test.EnableMockOAuth2Server
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
-import org.springframework.kafka.test.context.EmbeddedKafka
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.web.client.RestTemplate
 
 @SpringBootTest(classes = [RestTemplateConfig::class, UnsecuredWebMvcTestLauncher::class], webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles(profiles = ["excludeKodeverk","unsecured-webmvctest"])
-@EmbeddedKafka
-@EnableMockOAuth2Server
 @AutoConfigureMockMvc
 @MockkBeans(
-    MockkBean(name = "pdlRestTemplate", classes = [RestTemplate::class]),
-    MockkBean(name = "kodeverkClient", classes = [KodeverkClient::class], relaxed = true)
+    value = [
+        MockkBean(name = "pdlRestTemplate", classes = [RestTemplate::class]),
+        MockkBean(name = "kodeverkClient", classes = [KodeverkClient::class], relaxed = true),
+        MockkBean(name = "kafkaTemplate", classes = [KafkaTemplate::class], relaxed = true),
+    ]
 )
 class EessiApplicationConfigTest {
 
