b32ed5fe6a98e42f340dbf9bd4eea85c366115b4
Daniel Skarpås <dskarpas@gmail.com>
2026-02-11 15:21:35 +0100

    F - Legger til nye enum verdier for EessiKravAarsak

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/models/pensjon/EessiFellesDto.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/models/pensjon/EessiFellesDto.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 object EessiFellesDto { // brukes kun for namespace.
     enum class EessiSakType { AFP, AFP_PRIVAT, ALDER, BARNEP, FAM_PL, GAM_YRK, GENRL, GJENLEV, GRBL, KRIGSP, OMSORG, UFOREP }
     enum class EessiSakStatus { TIL_BEHANDLING, INGEN_STATUS, OPPRETTET, UKJENT, TRUKKET, AVBRUTT, AVSL, INNV, OPPHOR, VELG, VETIKKE, ANNET, EESSI_INGEN_STATUS, EESSI_UKJENT, EESSI_AVBRUTT}
-    enum class EessiKravAarsak { GJNL_SKAL_VURD, TILST_DOD, NY_SOKNAD, ANNET }
+    enum class EessiKravAarsak { GJNL_SKAL_VURD, TILST_DOD, NY_SOKNAD, ANNET, ENDRET_OPPTJENING, ANNEN_ARSAK }
     enum class EessiKravGjelder { F_BH_BO_UTL, F_BH_MED_UTL, F_BH_KUN_UTL, FORSTEG_BH, REVURD, SLUTT_BH_UTL, ANNET }
 
     data class PensjonSakDto(val sakId: String, val sakType: EessiSakType, val sakStatus: EessiSakStatus)
