62f14ad5c5bf884692a6cec7d47f09148e55b1f9
mariampervez <mariam.pervez@nav.no>
2026-02-17 10:05:02 +0100

    f - Legger på mer preutfylling av pensjonsobjektet for P2200

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/krav/PrefillP2200.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/sed/krav/PrefillP2200.kt
@@ -1,10 +1,12 @@
 package no.nav.eessi.pensjon.prefill.sed.krav
 
-import no.nav.eessi.pensjon.eux.model.sed.P2200
+import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.eux.model.sed.*
+import no.nav.eessi.pensjon.prefill.models.EessiInformasjon
 import no.nav.eessi.pensjon.prefill.models.PersonDataCollection
-
 import no.nav.eessi.pensjon.prefill.person.PrefillPDLNav
 import no.nav.eessi.pensjon.shared.api.PrefillDataModel
+import no.nav.eessi.pensjon.utils.toJson
 import no.nav.pensjon.v1.sak.V1Sak
 import no.nav.pensjon.v1.vedtak.V1Vedtak
 import org.slf4j.Logger
@@ -18,16 +20,21 @@ class PrefillP2200(private val prefillNav: PrefillPDLNav) {
 
     private val logger: Logger by lazy { LoggerFactory.getLogger(PrefillP2200::class.java) }
 
-    fun prefill(prefillData: PrefillDataModel, personData: PersonDataCollection, sak: V1Sak?, vedtak: V1Vedtak? = null) : P2200 {
+    fun prefill(
+        prefillData: PrefillDataModel,
+        personData: PersonDataCollection,
+        sak: V1Sak?,
+        vedtak: V1Vedtak? = null
+    ) : P2200 {
         logger.debug("----------------------------------------------------------"
-                + "\nSaktype                 : ${sak?.sakType} "
+                + "\nSaktype                 : ${sak?.toJson()} "
                 + "\nSøker etter SakId       : ${prefillData.penSaksnummer} "
                 + "\nSøker etter aktoerid    : ${prefillData.bruker.aktorId} "
                 + "\n------------------| Preutfylling [${prefillData.sedType}] START |------------------ ")
 
         val sedType = prefillData.sedType
 
-        val pensjon = PrefillP2xxxPensjon.populerPensjon(prefillData, sak)
+        val pensjon = populerPensjonP2200(prefillData, sak)
 
         //henter opp persondata
         val nav = prefillNav.prefill(
@@ -48,4 +55,68 @@ class PrefillP2200(private val prefillNav: PrefillPDLNav) {
             logger.debug("-------------------| Preutfylling [$sedType] END |------------------- ")
         }
     }
+
+    fun populerPensjonP2200(
+        prefillData: PrefillDataModel,
+        sak: V1Sak?
+    ): P2200Pensjon? {
+        val andreInstitusjondetaljer = EessiInformasjon().asAndreinstitusjonerItem()
+
+        logger.debug("""Prefilldata: ${prefillData.toJson()}""")
+
+        //valider pensjoninformasjon,
+        return try {
+            val pensjonsInformasjon: MeldingOmPensjonP2200 = PrefillP2xxxPensjon.populerMeldinOmPensjon(
+                prefillData.bruker.norskIdent,
+                prefillData.penSaksnummer,
+                sak,
+                andreInstitusjondetaljer
+            )
+
+            logger.debug("""Pensjoninformasjon: ${pensjonsInformasjon.toJson()}""")
+
+            if (prefillData.sedType != SedType.P6000) {
+                val ytelser = pensjonsInformasjon.pensjon.ytelser?.first()
+                val belop = ytelser?.beloep?.firstOrNull()
+
+                P2200Pensjon(
+                    kravDato = pensjonsInformasjon.pensjon.kravDato,
+                    ytelser = listOf(
+                        YtelserItem(
+                            ytelse = pensjonsInformasjon.pensjon.ytelser?.first()?.ytelse,
+                            status = ytelser?.status,
+                            startdatoutbetaling = ytelser?.startdatoutbetaling.also { logger.debug("startdatoUtbetaling: $it") },
+                            startdatoretttilytelse = ytelser?.startdatoretttilytelse.also { logger.debug("StartdatoretTilYtelseStatus: $it") },
+                            mottasbasertpaa = settMottattBasertPaa(ytelser?.totalbruttobeloeparbeidsbasert),
+                            totalbruttobeloepbostedsbasert = ytelser?.totalbruttobeloepbostedsbasert.also {
+                                logger.debug(
+                                    "totalbruttobeloepbostedsbasert: $it"
+                                )
+                            },
+                            totalbruttobeloeparbeidsbasert = ytelser?.totalbruttobeloeparbeidsbasert.also {
+                                logger.debug(
+                                    "totalbruttobeloeparbeidsbasert: $it"
+                                )
+                            },
+                            beloep = if (belop != null) listOf(belop) else null.also { logger.debug("beloep: $it") },
+                        )
+                    ),
+                    forespurtstartdato = pensjonsInformasjon.pensjon.forespurtstartdato.also { logger.debug("forespurtstartdato: $it") },
+                    etterspurtedokumenter = pensjonsInformasjon.pensjon.etterspurtedokumenter.also { logger.debug("etterspurtedokumenter: $it") },
+                )
+            } else pensjonsInformasjon.pensjon
+
+        } catch (ex: Exception) {
+            logger.error("Feilet ved preutfylling av pensjon, ${ex.message} ")
+            null
+            //hvis feiler lar vi SB få en SED i RINA
+        }
+    }
+
+    private fun settMottattBasertPaa(totalBruttoArbBasert: String?): String? {
+        return if (totalBruttoArbBasert.isNullOrEmpty() || totalBruttoArbBasert == "0") {
+            BasertPaa.botid.name
+        } else null
+    }
+
 }
