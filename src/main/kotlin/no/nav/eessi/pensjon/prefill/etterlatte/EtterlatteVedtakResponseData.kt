d73fff326eed2e4ae78eaa03027bf0c7caf7ea0d
mariampervez <mariam.pervez@nav.no>
2026-04-10 14:09:44 +0200

    b - Virkningsdato blir null dersom det er et avslag på vedtak
    Co-authored-by: Daniel.skarpas@nav.no

--- src/main/kotlin/no/nav/eessi/pensjon/prefill/etterlatte/EtterlatteVedtakResponseData.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/prefill/etterlatte/EtterlatteVedtakResponseData.kt
@@ -9,22 +9,25 @@ import no.nav.eessi.pensjon.eux.model.sed.VedtakItem
 data class EtterlatteVedtakResponseData(
     val vedtak: List<GjennyVedtak>
 ) {
-    fun hentVedtakItems(): List<VedtakItem> = vedtak.map { vedtak ->
-        VedtakItem(
-            virkningsdato = vedtak.virkningstidspunkt.toString(),
-            resultat = vedtak.type?.value,
-            type = "03", // Etterlatte pensjon
-            beregning = vedtak.utbetaling?.map {
-                BeregningItem(
-                    beloepBrutto = BeloepBrutto(beloep = it.beloep),
-                    periode = Periode(
-                        fom = it.fraOgMed.toString(),
-                        tom = it.tilOgMed?.toString() ?: ""
+    fun hentVedtakItems(): List<VedtakItem> {
+        return vedtak.map { vedtak ->
+            val avslElInnv = vedtak.type?.value
+            VedtakItem(
+                virkningsdato = if (avslElInnv == "01") vedtak.virkningstidspunkt.toString() else null,
+                resultat = avslElInnv,
+                type = "03", // Etterlatte pensjon
+                beregning = vedtak.utbetaling?.map {
+                    BeregningItem(
+                        beloepBrutto = BeloepBrutto(beloep = it.beloep),
+                        periode = Periode(
+                            fom = it.fraOgMed.toString(),
+                            tom = it.tilOgMed?.toString() ?: ""
+                        )
                     )
-                )
-            },
-            iverksettelsesTidspunkt = vedtak.iverksettelsesTidspunkt?.toLocalDate()
-        )
+                },
+                iverksettelsesTidspunkt = vedtak.iverksettelsesTidspunkt?.toLocalDate()
+            )
+        }
     }
 
     fun hentFørsteSakType(): String? = vedtak.firstOrNull { it.sakType != null }?.sakType
