961a670e8892b2868912db8e4548a84017f0f7c3
Daniel Skarpås <dskarpas@gmail.com>
2026-06-26 10:29:58 +0200

    r - Oppdatering av apiRequest, sletter unødvendig gjenny-sjekk (epd)

--- src/main/kotlin/no/nav/eessi/pensjon/shared/api/ApiRequest.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/shared/api/ApiRequest.kt
@@ -115,7 +115,6 @@ data class ApiRequest(
 
         private fun populerAvdodPersonId(request: ApiRequest, avdodaktoerID: String?, kreverAvdod: Boolean = false): PersonInfo? {
             if (kreverAvdod && avdodaktoerID == null) {
-                if(request.gjenny) return null
                 logger.error("Mangler fnr for avdød")
                 if (request.gjenny) {
                     return null
